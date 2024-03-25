436cd8390b64ab42b0e87585ab8f1143f5156f78
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-03-25 16:23:53 +0900

    Generate Rust code from proto files

--- rust/ommx/src/lib.rs
+++ rust/ommx/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! Open Mathematics prograMming eXchange (OMMX)
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod ommx;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use ommx::*;
--- rust/protogen/src/main.rs
+++ rust/protogen/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use prost_build::Config;
+use std::path::Path;
+
+fn main() -> Result<()> {
+    let manifest_root: &Path = env!("CARGO_MANIFEST_DIR").as_ref();
+    let repo_root = manifest_root.join("../..").canonicalize()?;
+    let proto_root = repo_root.join("protobuf");
+    dbg!(&manifest_root, &repo_root, &proto_root);
+
+    let protos = ["polynomial.proto"]
+        .iter()
+        .map(|p| proto_root.join(p))
+        .collect::<Vec<_>>();
+
+    let mut cfg = Config::new();
+    cfg.out_dir(repo_root.join("rust/ommx/src"))
+        .compile_protos(&protos, &[proto_root])?;
+    Ok(())
 }
