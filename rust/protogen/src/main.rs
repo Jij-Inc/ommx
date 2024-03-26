f55391b8749c24ecce4f548359a7c0b9b8e18732
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-03-26 15:38:40 +0900

    Schema for decision variable attributes, better protogen (#2)

--- rust/protogen/src/main.rs
+++ rust/protogen/src/main.rs
@@ -1,20 +1,46 @@
 use anyhow::Result;
+use colored::Colorize;
 use prost_build::Config;
-use std::path::Path;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 fn main() -> Result<()> {
     let manifest_root: &Path = env!("CARGO_MANIFEST_DIR").as_ref();
     let repo_root = manifest_root.join("../..").canonicalize()?;
     let proto_root = repo_root.join("protobuf");
-    dbg!(&manifest_root, &repo_root, &proto_root);
 
-    let protos = ["polynomial.proto"]
-        .iter()
-        .map(|p| proto_root.join(p))
-        .collect::<Vec<_>>();
+    let protos = fs::read_dir(&proto_root)?
+        .map(|entry| -> Result<PathBuf> { Ok(entry?.path()) })
+        .collect::<Result<Vec<_>>>()?;
+
+    eprintln!(
+        "{:>12} in {}",
+        "Proto files".bold().cyan(),
+        proto_root.display()
+    );
+    for proto in &protos {
+        eprintln!(
+            "{:>12} - {}",
+            "",
+            proto.file_name().unwrap().to_string_lossy()
+        );
+    }
+
+    let out = repo_root.join("rust/ommx/src");
+    eprintln!(
+        "{:>12} {}",
+        "Writing".bold().cyan(),
+        out.join("ommx.rs").display()
+    );
 
     let mut cfg = Config::new();
-    cfg.out_dir(repo_root.join("rust/ommx/src"))
-        .compile_protos(&protos, &[proto_root])?;
+    cfg.out_dir(&out).compile_protos(&protos, &[proto_root])?;
+
+    std::process::Command::new("rustfmt")
+        .arg(out.join("ommx.rs"))
+        .status()?;
+
     Ok(())
 }
