fe7ffe04c477329932e590071d52416e00546830
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-03-29 20:17:39 +0900

    Regenerate Rust code

--- rust/ommx/src/ommx.v1.rs
+++ rust/ommx/src/ommx.v1.rs
@@ -1,3 +1,134 @@
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Linear {
+    #[prost(message, repeated, tag = "1")]
+    pub terms: ::prost::alloc::vec::Vec<linear::Term>,
+}
+/// Nested message and enum types in `Linear`.
+pub mod linear {
+    #[allow(clippy::derive_partial_eq_without_eq)]
+    #[derive(Clone, PartialEq, ::prost::Message)]
+    pub struct Term {
+        #[prost(uint64, tag = "1")]
+        pub id: u64,
+        #[prost(double, tag = "2")]
+        pub coefficient: f64,
+    }
+}
+/// A monomial in a multivariate polynomial.
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Monomial {
+    #[prost(uint64, repeated, tag = "1")]
+    pub ids: ::prost::alloc::vec::Vec<u64>,
+    #[prost(fixed64, tag = "2")]
+    pub coefficient: u64,
+}
+/// Multi­variate polynomial
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Polynomial {
+    #[prost(message, repeated, tag = "1")]
+    pub terms: ::prost::alloc::vec::Vec<Monomial>,
+}
+/// COO format of sparse matrix. The components must have same size.
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct SparseMatrix {
+    #[prost(uint64, repeated, tag = "1")]
+    pub rows: ::prost::alloc::vec::Vec<u64>,
+    #[prost(uint64, repeated, tag = "2")]
+    pub columns: ::prost::alloc::vec::Vec<u64>,
+    #[prost(double, repeated, tag = "3")]
+    pub values: ::prost::alloc::vec::Vec<f64>,
+}
+/// Quadratic function as a sparse matrix and linear sparse vector.
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Quadratic {
+    #[prost(message, optional, tag = "1")]
+    pub quadradic: ::core::option::Option<SparseMatrix>,
+    #[prost(message, optional, tag = "2")]
+    pub linear: ::core::option::Option<Linear>,
+}
+/// Real-valued multivariate function used for objective function and constraints.
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Function {
+    #[prost(oneof = "function::Function", tags = "1, 2, 3, 4")]
+    pub function: ::core::option::Option<function::Function>,
+}
+/// Nested message and enum types in `Function`.
+pub mod function {
+    #[allow(clippy::derive_partial_eq_without_eq)]
+    #[derive(Clone, PartialEq, ::prost::Oneof)]
+    pub enum Function {
+        /// Constant function like `f(x_1, x_2) = 2`
+        #[prost(double, tag = "1")]
+        Constant(f64),
+        /// Linear function like `f(x_1, x_2) = 2 x_1 + 3 x_2`
+        #[prost(message, tag = "2")]
+        Linear(super::Linear),
+        /// Quadratic function like `f(x_1, x_2) = 4 x_1 x_2 + 5 x_2`
+        #[prost(message, tag = "3")]
+        Quadratic(super::Quadratic),
+        /// Polynomial like `f(x_1, x_2) = 4 x_1^2 + 5 x_2^3 + 6 x_1 x_2^2 + 7 x_2^2 + 8 x_1 x_2 + 9 x_1 + 10 x_2 + 11`
+        #[prost(message, tag = "4")]
+        Polynomial(super::Polynomial),
+    }
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Constraint {
+    #[prost(uint64, tag = "1")]
+    pub id: u64,
+    #[prost(enumeration = "constraint::Equality", tag = "2")]
+    pub equality: i32,
+    #[prost(message, optional, tag = "3")]
+    pub function: ::core::option::Option<Function>,
+    #[prost(message, optional, tag = "4")]
+    pub description: ::core::option::Option<constraint::Description>,
+}
+/// Nested message and enum types in `Constraint`.
+pub mod constraint {
+    #[allow(clippy::derive_partial_eq_without_eq)]
+    #[derive(Clone, PartialEq, ::prost::Message)]
+    pub struct Description {
+        #[prost(string, tag = "1")]
+        pub name: ::prost::alloc::string::String,
+        #[prost(uint64, repeated, tag = "2")]
+        pub forall: ::prost::alloc::vec::Vec<u64>,
+    }
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+    #[repr(i32)]
+    pub enum Equality {
+        Unspecified = 0,
+        EqualToZero = 1,
+        LessThanOrEqualToZero = 2,
+    }
+    impl Equality {
+        /// String value of the enum field names used in the ProtoBuf definition.
+        ///
+        /// The values are not transformed in any way and thus are considered stable
+        /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+        pub fn as_str_name(&self) -> &'static str {
+            match self {
+                Equality::Unspecified => "EQUALITY_UNSPECIFIED",
+                Equality::EqualToZero => "EQUALITY_EQUAL_TO_ZERO",
+                Equality::LessThanOrEqualToZero => "EQUALITY_LESS_THAN_OR_EQUAL_TO_ZERO",
+            }
+        }
+        /// Creates an enum from field names used in the ProtoBuf definition.
+        pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+            match value {
+                "EQUALITY_UNSPECIFIED" => Some(Self::Unspecified),
+                "EQUALITY_EQUAL_TO_ZERO" => Some(Self::EqualToZero),
+                "EQUALITY_LESS_THAN_OR_EQUAL_TO_ZERO" => Some(Self::LessThanOrEqualToZero),
+                _ => None,
+            }
+        }
+    }
+}
 /// Decison variable which mathematical programming solver will optimize.
 /// It must have its kind, i.e. binary, integer, real or others and unique identifier of 64-bit integer.
 /// It may have its name and subscripts which are used to identify in modeling tools.
@@ -57,73 +188,6 @@ pub mod decision_variable {
                 "KIND_BINARY" => Some(Self::Binary),
                 "KIND_INTEGER" => Some(Self::Integer),
                 "KIND_REAL" => Some(Self::Real),
-                _ => None,
-            }
-        }
-    }
-}
-/// A monomial in a multivariate polynomial.
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Monomial {
-    #[prost(uint64, repeated, tag = "1")]
-    pub ids: ::prost::alloc::vec::Vec<u64>,
-    #[prost(fixed64, tag = "2")]
-    pub coefficient: u64,
-}
-/// Multi­variate polynomial
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Polynomial {
-    #[prost(message, repeated, tag = "1")]
-    pub terms: ::prost::alloc::vec::Vec<Monomial>,
-}
-/// Real-valued multivariate function used for objective function and constraints.
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Function {
-    #[prost(enumeration = "function::Kind", tag = "1")]
-    pub kind: i32,
-    #[prost(double, optional, tag = "2")]
-    pub constant: ::core::option::Option<f64>,
-    /// optional Linear linear = 3;
-    /// optional Quadratic quadratic = 4;
-    #[prost(message, optional, tag = "5")]
-    pub polynomial: ::core::option::Option<Polynomial>,
-}
-/// Nested message and enum types in `Function`.
-pub mod function {
-    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
-    #[repr(i32)]
-    pub enum Kind {
-        Unspecified = 0,
-        Constant = 1,
-        Linear = 2,
-        Quadratic = 3,
-        Polynomial = 4,
-    }
-    impl Kind {
-        /// String value of the enum field names used in the ProtoBuf definition.
-        ///
-        /// The values are not transformed in any way and thus are considered stable
-        /// (if the ProtoBuf definition does not change) and safe for programmatic use.
-        pub fn as_str_name(&self) -> &'static str {
-            match self {
-                Kind::Unspecified => "KIND_UNSPECIFIED",
-                Kind::Constant => "KIND_CONSTANT",
-                Kind::Linear => "KIND_LINEAR",
-                Kind::Quadratic => "KIND_QUADRATIC",
-                Kind::Polynomial => "KIND_POLYNOMIAL",
-            }
-        }
-        /// Creates an enum from field names used in the ProtoBuf definition.
-        pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
-            match value {
-                "KIND_UNSPECIFIED" => Some(Self::Unspecified),
-                "KIND_CONSTANT" => Some(Self::Constant),
-                "KIND_LINEAR" => Some(Self::Linear),
-                "KIND_QUADRATIC" => Some(Self::Quadratic),
-                "KIND_POLYNOMIAL" => Some(Self::Polynomial),
                 _ => None,
             }
         }
