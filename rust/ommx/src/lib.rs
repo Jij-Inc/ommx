acda08eab1bb381d0c87e38adbf2727b628e1d81
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-04-01 21:14:35 +0900

    Add wrapper of ommx::v1::Instance::decode

--- rust/ommx/src/lib.rs
+++ rust/ommx/src/lib.rs
@@ -1,6 +1,21 @@
 //! Open Mathematics prograMming eXchange (OMMX)
 
+use prost::Message;
+
 /// Module created from `ommx.v1` proto files
 pub mod v1 {
     include!("ommx.v1.rs");
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    DecodeError(#[from] prost::DecodeError),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+/// Decode a [`v1::Instance`] from a byte slice
+pub fn decode_instance(buf: &[u8]) -> Result<v1::Instance> {
+    Ok(v1::Instance::decode(buf)?)
 }
