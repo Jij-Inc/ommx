f378416deba0b88fb48d8dcff177b0950ee16422
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-04-01 13:14:28 +0900

    Rename namespace for generated Rust bindings

--- rust/ommx/src/lib.rs
+++ rust/ommx/src/lib.rs
@@ -1,9 +1,6 @@
 //! Open Mathematics prograMming eXchange (OMMX)
 
-pub(crate) mod ommx {
-    pub(crate) mod v1 {
-        include!("ommx.v1.rs");
-    }
+/// Module created from `ommx.v1` proto files
+pub mod v1 {
+    include!("ommx.v1.rs");
 }
-
-pub use ommx::v1::*;
