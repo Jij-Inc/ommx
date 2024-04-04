a27ad1f3b3f61291ff9ffb63d94cb895c8a13921
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-04-04 19:50:38 +0900

    Drop decode_instance, directly use v1::Instance::decode

--- rust/ommx/src/lib.rs
+++ rust/ommx/src/lib.rs
@@ -6,16 +6,3 @@ pub use prost::Message;
 pub mod v1 {
     include!("ommx.v1.rs");
 }
-
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-    #[error(transparent)]
-    DecodeError(#[from] prost::DecodeError),
-}
-
-pub type Result<T> = std::result::Result<T, Error>;
-
-/// Decode a [`v1::Instance`] from a byte slice
-pub fn decode_instance(buf: &[u8]) -> Result<v1::Instance> {
-    Ok(v1::Instance::decode(buf)?)
-}
