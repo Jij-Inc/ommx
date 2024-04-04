19f2855b8f6ed375a416742cd90a3208bca4de71
NY57 <nishimuray.theory57@gmail.com>
2024-04-04 15:01:41 +0900

    resolve conversations

--- python/tests/test_import.py
+++ python/tests/test_import.py
@@ -1,12 +1,2 @@
 def test_import():
-    from ommx.v1 import (
-        constraint_pb2,
-        decision_variables_pb2,
-        function_pb2,
-        instance_pb2,
-        linear_pb2,
-        polynomial_pb2,
-        quadratic_pb2,
-        solution_pb2,
-        sparse_matrix_pb2,
-    )
+    from ommx.v1 import instance_pb2
