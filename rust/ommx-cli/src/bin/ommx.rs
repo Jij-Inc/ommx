5e8973e9645dbbbd615b8f3084a35dbc4e15ea0d
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-04-10 14:38:41 +0900

    Mock `ommx inspect` command

--- rust/ommx-cli/src/bin/ommx.rs
+++ rust/ommx-cli/src/bin/ommx.rs
@@ -1,4 +1,6 @@
 use clap::{Parser, Subcommand};
+use ocipkg::ImageName;
+use std::path::Path;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -12,7 +14,33 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Inspect { image_name_or_path: String },
+    Inspect {
+        /// Container image name or the path of OCI archive
+        image_name_or_path: String,
+    },
+}
+
+fn inspect(image_name_or_path: &str) {
+    let path: &Path = image_name_or_path.as_ref();
+    if path.exists() {
+        log::debug!("Regarded as a filesystem path: {}", path.display());
+        if !path.is_file() {
+            panic!("Not a file: {}", path.display());
+        }
+
+        dbg!(path);
+
+        return;
+    }
+    if path.is_file() {
+        dbg!("It is a file");
+        return;
+    }
+    if let Ok(image) = ImageName::parse(image_name_or_path) {
+        dbg!(image);
+        return;
+    };
+    panic!("Not an image name or valid path: {}", image_name_or_path);
 }
 
 fn main() {
@@ -27,7 +55,7 @@ fn main() {
 
     match &cli.command {
         Some(Commands::Inspect { image_name_or_path }) => {
-            dbg!(image_name_or_path);
+            inspect(image_name_or_path);
         }
         None => {}
     }
