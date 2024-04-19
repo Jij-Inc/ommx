3387f8def19aaa44a4393871c9c4797850e3e005
NY57 <nishimuray.theory57@gmail.com>
2024-04-19 14:43:03 +0900

    add test_generator

---
+++ python/tests/test_generator.py
@@ -0,0 +1,12 @@
+from ommx.v1._test_generator import LPTestDataType, LPTestGenerater
+from ommx.v1.instance_pb2 import Instance
+from ommx.v1.solution_pb2 import SolutionList
+
+
+def test_generator():
+    generator = LPTestGenerater(3, LPTestDataType.INT)
+    ommx_instance_byte = generator.get_instance()
+    ommx_solution_byte = generator.get_solution()
+
+    Instance().ParseFromString(ommx_instance_byte)
+    SolutionList().ParseFromString(ommx_solution_byte)
