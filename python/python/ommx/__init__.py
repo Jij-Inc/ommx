2444372505fdc088b0532736203e5d601871ed16
NY57 <nishimuray.theory57@gmail.com>
2024-04-26 17:17:56 +0900

    fix

--- python/python/ommx/__init__.py
+++ python/python/ommx/__init__.py
@@ -1,6 +1,1 @@
-from .ommx import *
-
-
-__doc__ = ommx.__doc__    # type: ignore
-if hasattr(ommx, "__all__"):    # type: ignore
-    __all__ = ommx.__all__    # type: ignore
+from ._ommx_rust import *
