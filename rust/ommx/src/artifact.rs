258779eea606cdb8ba6dae61e11a996445d8c7c2
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-10 18:00:57 +0900

    Builder

--- rust/ommx/src/artifact.rs
+++ rust/ommx/src/artifact.rs
@@ -4,23 +4,43 @@
 mod media_type;
 pub use media_type::*;
 
-use ocipkg::image::{Image, OciArtifact};
-use std::ops::{Deref, DerefMut};
+use anyhow::{bail, Result};
+use ocipkg::image::{Image, ImageBuilder, OciArchiveBuilder, OciArtifact, OciArtifactBuilder};
+use std::{
+    ops::{Deref, DerefMut},
+    path::Path,
+};
 
 /// OCI Artifact of artifact type [`application/vnd.ommx.v1.artifact`][v1_artifact]
-pub struct Artifact<Base: Image> {
-    base: OciArtifact<Base>,
-}
+pub struct Artifact<Base: Image>(OciArtifact<Base>);
 
 impl<Base: Image> Deref for Artifact<Base> {
     type Target = OciArtifact<Base>;
     fn deref(&self) -> &Self::Target {
-        &self.base
+        &self.0
     }
 }
 
 impl<Base: Image> DerefMut for Artifact<Base> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.base
+        &mut self.0
+    }
+}
+
+/// Build [Artifact]
+pub struct Builder<Base: ImageBuilder>(OciArtifactBuilder<Base>);
+
+impl Builder<OciArchiveBuilder> {
+    pub fn new_archive(path: &Path) -> Result<Self> {
+        if path.exists() {
+            bail!("File already exists: {}", path.display());
+        }
+        todo!()
+    }
+}
+
+impl<Base: ImageBuilder> Builder<Base> {
+    pub fn build(self) -> Result<Artifact<Base::Image>> {
+        Ok(Artifact(self.0.build()?))
     }
 }
