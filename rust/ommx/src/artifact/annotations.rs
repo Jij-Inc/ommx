7bc70f4e337aaaa8916fd43de044effc989e283a
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-16 14:53:41 +0900

    Start and end time

--- rust/ommx/src/artifact/annotations.rs
+++ rust/ommx/src/artifact/annotations.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Result};
+use chrono::{DateTime, Local};
 use derive_more::{Deref, From, Into};
 use ocipkg::Digest;
 use serde::{Deserialize, Serialize};
@@ -18,11 +19,46 @@ impl InstanceAnnotations {
 }
 
 /// Annotations for [`application/org.ommx.v1.solution`][crate::artifact::media_type::v1_solution]
+///
+/// Annotations
+/// ------------
+/// - `org.ommx.v1.solution.instance`: The digest of the corresponding instance of the solution
+/// - `org.ommx.v1.solution.solver`: The digest of the solver information which generated the solution
+/// - `org.ommx.v1.solution.parameters`: Solver parameters used to generate the solution as a JSON
+/// - `org.ommx.v1.solution.start`: The start time of the solution as a RFC3339 string
+/// - `org.ommx.v1.solution.end`: The end time of the solution as a RFC3339 string
+///
+/// In addition, other annotations are allowed. The key may not start with `org.ommx.v1.`, but must be a valid reverse domain name.
+///
 #[derive(Debug, Default, Clone, PartialEq, From, Deref, Into)]
 pub struct SolutionAnnotations(HashMap<String, String>);
 
 impl SolutionAnnotations {
-    /// Set the value of [Self::instance]
+    pub fn set_start(&mut self, start: DateTime<Local>) {
+        self.0
+            .insert("org.ommx.v1.solution.start".to_string(), start.to_rfc3339());
+    }
+
+    pub fn start(&self) -> Result<DateTime<Local>> {
+        let start = self.0.get("org.ommx.v1.solution.start").context(
+            "Annotation does not have the entry with the key `org.ommx.v1.solution.start`",
+        )?;
+        Ok(DateTime::parse_from_rfc3339(start)?.with_timezone(&Local))
+    }
+
+    pub fn set_end(&mut self, end: DateTime<Local>) {
+        self.0
+            .insert("org.ommx.v1.solution.end".to_string(), end.to_rfc3339());
+    }
+
+    pub fn end(&self) -> Result<DateTime<Local>> {
+        let end = self.0.get("org.ommx.v1.solution.end").context(
+            "Annotation does not have the entry with the key `org.ommx.v1.solution.end`",
+        )?;
+        Ok(DateTime::parse_from_rfc3339(end)?.with_timezone(&Local))
+    }
+
+    /// Set `org.ommx.v1.solution.instance`
     pub fn set_instance(&mut self, digest: Digest) {
         self.0.insert(
             "org.ommx.v1.solution.instance".to_string(),
@@ -30,7 +66,7 @@ impl SolutionAnnotations {
         );
     }
 
-    /// A reference to the instance of the solution stored with the key `org.ommx.v1.solution.instance`
+    /// Get `org.ommx.v1.solution.instance`
     pub fn instance(&self) -> Result<Digest> {
         let digest = self.0.get("org.ommx.v1.solution.instance").context(
             "Annotation does not have the entry with the key `org.ommx.v1.solution.instance`",
@@ -38,7 +74,7 @@ impl SolutionAnnotations {
         Ok(Digest::new(digest)?)
     }
 
-    /// Set the value of [Self::solver]
+    /// Set `org.ommx.v1.solution.solver`
     pub fn set_solver(&mut self, digest: Digest) {
         self.0.insert(
             "org.ommx.v1.solution.solver".to_string(),
@@ -46,7 +82,7 @@ impl SolutionAnnotations {
         );
     }
 
-    /// A reference to the solver information which generated the solution stored with the key `org.ommx.v1.solution.solver`
+    /// Get `org.ommx.v1.solution.solver`
     pub fn solver(&self) -> Result<Digest> {
         let digest = self.0.get("org.ommx.v1.solution.solver").context(
             "Annotation does not have the entry with the key `org.ommx.v1.solution.solver`",
@@ -54,7 +90,7 @@ impl SolutionAnnotations {
         Ok(Digest::new(digest)?)
     }
 
-    /// Set the value of [Self::parameters]
+    /// Set `org.ommx.v1.solution.parameters`
     pub fn set_parameters(&mut self, parameters: impl Serialize) -> Result<()> {
         self.0.insert(
             "org.ommx.v1.solution.parameters".to_string(),
@@ -63,7 +99,7 @@ impl SolutionAnnotations {
         Ok(())
     }
 
-    /// Solver parameters used to generate the solution as a JSON with the key `org.ommx.v1.solution.parameters`
+    /// Get `org.ommx.v1.solution.parameters`
     pub fn parameters<'s: 'de, 'de, P: Deserialize<'de>>(&'s self) -> Result<P> {
         Ok(serde_json::from_str(
             self.0.get("org.ommx.v1.solution.parameters").context(
@@ -72,7 +108,7 @@ impl SolutionAnnotations {
         )?)
     }
 
-    /// Set other annotations. The key may not start with `org.ommx.v1.`, but must be a valid reverse domain name.
+    /// Set other annotations
     pub fn set_other(&mut self, key: String, value: String) {
         // TODO check key
         self.0.insert(key, value);
