f61e6239e8d3eefee0043712c894e3ae7bd78dee
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-16 21:04:16 +0900

    Setup workspace

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,4 +1,16 @@
+use anyhow::Result;
 use pyo3::prelude::*;
+
+#[pyclass]
+#[pyo3(module = "ommx._ommx_rust")]
+pub struct Artifact(ommx::artifact::Artifact<ocipkg::image::OciArchive>);
+
+#[pymethods]
+impl Artifact {
+    pub fn num_instances(&mut self) -> Result<usize> {
+        Ok(self.0.get_instances()?.len())
+    }
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -10,5 +22,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn _ommx_rust(_py: Python, m: Bound<PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, &m)?)?;
+    m.add_class::<Artifact>()?;
     Ok(())
 }
