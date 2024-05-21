e860073918e60e34db49b200ff16357a2cca443f
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-21 20:41:16 +0900

    Store used variable ids in the evaluation result

--- rust/ommx/src/evaluate.rs
+++ rust/ommx/src/evaluate.rs
@@ -3,6 +3,7 @@ use crate::v1::{
     EvaluatedConstraint, Function, Instance, Linear, Polynomial, Quadratic, Solution, State,
 };
 use anyhow::{bail, Context, Result};
+use std::collections::BTreeSet;
 
 /// Evaluate with a [State]
 ///
@@ -23,19 +24,22 @@ use anyhow::{bail, Context, Result};
 ///     .into_iter(),
 ///     3.0,
 /// );
-/// assert_eq!(linear.evaluate(&raw)?, 1.0 * 1.0 + 2.0 * 2.0 + 3.0);
+/// let (value, used_ids) = linear.evaluate(&raw)?;
+/// assert_eq!(value, 1.0 * 1.0 + 2.0 * 2.0 + 3.0);
+/// assert_eq!(used_ids, vec![1, 2].into_iter().collect());
 /// # Ok(()) }
 /// ```
 pub trait Evaluate {
     type Output;
-    fn evaluate(&self, solution: &State) -> Result<Self::Output>;
+    /// Evaluate to return the output with used variable ids
+    fn evaluate(&self, solution: &State) -> Result<(Self::Output, BTreeSet<u64>)>;
 }
 
 impl Evaluate for Function {
     type Output = f64;
-    fn evaluate(&self, solution: &State) -> Result<f64> {
+    fn evaluate(&self, solution: &State) -> Result<(f64, BTreeSet<u64>)> {
         let out = match &self.function {
-            Some(FunctionEnum::Constant(c)) => *c,
+            Some(FunctionEnum::Constant(c)) => (*c, BTreeSet::new()),
             Some(FunctionEnum::Linear(linear)) => linear.evaluate(solution)?,
             Some(FunctionEnum::Quadratic(quadratic)) => quadratic.evaluate(solution)?,
             Some(FunctionEnum::Polynomial(poly)) => poly.evaluate(solution)?,
@@ -47,30 +51,35 @@ impl Evaluate for Function {
 
 impl Evaluate for Linear {
     type Output = f64;
-    fn evaluate(&self, solution: &State) -> Result<f64> {
+    fn evaluate(&self, solution: &State) -> Result<(f64, BTreeSet<u64>)> {
         let mut sum = self.constant;
+        let mut used_ids = BTreeSet::new();
         for LinearTerm { id, coefficient } in &self.terms {
+            used_ids.insert(*id);
             let s = solution
                 .entries
                 .get(id)
                 .with_context(|| format!("Variable id ({id}) is not found in the solution"))?;
             sum += coefficient * s;
         }
-        Ok(sum)
+        Ok((sum, used_ids))
     }
 }
 
 impl Evaluate for Quadratic {
     type Output = f64;
-    fn evaluate(&self, solution: &State) -> Result<f64> {
-        let mut sum = if let Some(linear) = &self.linear {
+    fn evaluate(&self, solution: &State) -> Result<(f64, BTreeSet<u64>)> {
+        let (mut sum, mut used_ids) = if let Some(linear) = &self.linear {
             linear.evaluate(solution)?
         } else {
-            0.0
+            (0.0, BTreeSet::new())
         };
         for (i, j, value) in
             itertools::multizip((self.rows.iter(), self.columns.iter(), self.values.iter()))
         {
+            used_ids.insert(*i);
+            used_ids.insert(*j);
+
             let u = solution
                 .entries
                 .get(i)
@@ -81,17 +90,19 @@ impl Evaluate for Quadratic {
                 .with_context(|| format!("Variable id ({j}) is not found in the solution"))?;
             sum += value * u * v;
         }
-        Ok(sum)
+        Ok((sum, used_ids))
     }
 }
 
 impl Evaluate for Polynomial {
     type Output = f64;
-    fn evaluate(&self, solution: &State) -> Result<f64> {
+    fn evaluate(&self, solution: &State) -> Result<(f64, BTreeSet<u64>)> {
         let mut sum = 0.0;
+        let mut used_ids = BTreeSet::new();
         for term in &self.terms {
             let mut v = term.coefficient;
             for id in &term.ids {
+                used_ids.insert(*id);
                 v *= solution
                     .entries
                     .get(id)
@@ -99,39 +110,43 @@ impl Evaluate for Polynomial {
             }
             sum += v;
         }
-        Ok(sum)
+        Ok((sum, used_ids))
     }
 }
 
 impl Evaluate for Constraint {
     type Output = EvaluatedConstraint;
 
-    fn evaluate(&self, solution: &State) -> Result<Self::Output> {
-        let evaluated_value = self
+    fn evaluate(&self, solution: &State) -> Result<(Self::Output, BTreeSet<u64>)> {
+        let (evaluated_value, used_ids) = self
             .function
             .as_ref()
             .context("Function is not set")?
             .evaluate(solution)?;
-        Ok(EvaluatedConstraint {
-            id: self.id,
-            equality: self.equality,
-            evaluated_value,
-            description: self.description.clone(),
-        })
+        let used_decision_variable_ids = used_ids.iter().cloned().collect();
+        Ok((
+            EvaluatedConstraint {
+                id: self.id,
+                equality: self.equality,
+                evaluated_value,
+                used_decision_variable_ids,
+                description: self.description.clone(),
+            },
+            used_ids,
+        ))
     }
 }
 
 impl Evaluate for Instance {
     type Output = Solution;
 
-    fn evaluate(&self, state: &State) -> Result<Self::Output> {
-        let evaluated_constraints = self
-            .constraints
-            .iter()
-            .map(|c| c.evaluate(state))
-            .collect::<Result<Vec<_>>>()?;
+    fn evaluate(&self, state: &State) -> Result<(Self::Output, BTreeSet<u64>)> {
+        let mut used_ids = BTreeSet::new();
+        let mut evaluated_constraints = Vec::new();
         let mut feasible = true;
-        for c in &evaluated_constraints {
+        for c in &self.constraints {
+            let (c, used_ids_) = c.evaluate(state)?;
+            used_ids.extend(used_ids_);
             if c.equality == Equality::EqualToZero as i32 {
                 // FIXME: Add a way to specify the tolerance
                 if c.evaluated_value.abs() > 1e-6 {
@@ -146,20 +161,25 @@ impl Evaluate for Instance {
             } else {
                 bail!("Unsupported equality: {:?}", c.equality);
             }
+            evaluated_constraints.push(c);
         }
 
-        let objective = self
+        let (objective, used_ids_) = self
             .objective
             .as_ref()
             .context("Objective is not set")?
             .evaluate(state)?;
-        Ok(Solution {
-            decision_variables: self.decision_variables.clone(),
-            state: Some(state.clone()),
-            evaluated_constraints,
-            feasible,
-            objective,
-            optimal: None,
-        })
+        used_ids.extend(used_ids_);
+        Ok((
+            Solution {
+                decision_variables: self.decision_variables.clone(),
+                state: Some(state.clone()),
+                evaluated_constraints,
+                feasible,
+                objective,
+                optimal: None,
+            },
+            used_ids,
+        ))
     }
 }
