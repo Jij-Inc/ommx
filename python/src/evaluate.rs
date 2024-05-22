924282170078e8a41a6524ab09d1c04030144e5d
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-22 13:53:04 +0900

    Evaluate more

--- python/src/evaluate.rs
+++ python/src/evaluate.rs
@@ -1,17 +1,45 @@
 use anyhow::Result;
 use ommx::{
-    v1::{Function, State},
+    v1::{Constraint, Function, Instance, Linear, Polynomial, Quadratic, State},
     Evaluate, Message,
 };
 use pyo3::{prelude::*, types::PyBytes};
 use std::collections::BTreeSet;
 
-#[pyfunction]
-pub fn evaluate_function<'py>(
-    function: &Bound<'py, PyBytes>,
-    state: &Bound<'py, PyBytes>,
-) -> Result<(f64, BTreeSet<u64>)> {
-    let state = State::decode(state.as_bytes())?;
-    let function = Function::decode(function.as_bytes())?;
-    function.evaluate(&state)
+macro_rules! define_evaluate_function {
+    ($evaluated:ty, $name:ident) => {
+        #[pyfunction]
+        pub fn $name<'py>(
+            function: &Bound<'py, PyBytes>,
+            state: &Bound<'py, PyBytes>,
+        ) -> Result<(f64, BTreeSet<u64>)> {
+            let state = State::decode(state.as_bytes())?;
+            let function = <$evaluated>::decode(function.as_bytes())?;
+            function.evaluate(&state)
+        }
+    };
 }
+
+define_evaluate_function!(Function, evaluate_function);
+define_evaluate_function!(Linear, evaluate_linear);
+define_evaluate_function!(Quadratic, evaluate_quadratic);
+define_evaluate_function!(Polynomial, evaluate_polynomial);
+
+macro_rules! define_evaluate_object {
+    ($evaluated:ty, $name:ident) => {
+        #[pyfunction]
+        pub fn $name<'py>(
+            py: Python<'py>,
+            function: &Bound<'py, PyBytes>,
+            state: &Bound<'py, PyBytes>,
+        ) -> Result<(Bound<'py, PyBytes>, BTreeSet<u64>)> {
+            let state = State::decode(state.as_bytes())?;
+            let function = <$evaluated>::decode(function.as_bytes())?;
+            let (evaluated, used_ids) = function.evaluate(&state)?;
+            Ok((PyBytes::new_bound(py, &evaluated.encode_to_vec()), used_ids))
+        }
+    };
+}
+
+define_evaluate_object!(Constraint, evaluate_constraint);
+define_evaluate_object!(Instance, evaluate_instance);
--- python/src/lib.rs
+++ python/src/lib.rs
@@ -10,5 +10,10 @@ fn _ommx_rust(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add_class::<Artifact>()?;
     m.add_class::<Descriptor>()?;
     m.add_function(wrap_pyfunction!(evaluate_function, m)?)?;
+    m.add_function(wrap_pyfunction!(evaluate_linear, m)?)?;
+    m.add_function(wrap_pyfunction!(evaluate_quadratic, m)?)?;
+    m.add_function(wrap_pyfunction!(evaluate_polynomial, m)?)?;
+    m.add_function(wrap_pyfunction!(evaluate_constraint, m)?)?;
+    m.add_function(wrap_pyfunction!(evaluate_instance, m)?)?;
     Ok(())
 }
