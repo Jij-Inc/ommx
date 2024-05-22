a9c9f1c615f41b6d9c0c09d316da7b1969eeb9df
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-22 11:57:53 +0900

    _ommx_rust.evaluate_function

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,8 +1,15 @@
 use anyhow::Result;
 use ocipkg::{image::OciArchive, oci_spec::image::Descriptor as RawDescriptor};
-use ommx::artifact::media_types;
-use pyo3::prelude::*;
-use std::{collections::HashMap, path::PathBuf};
+use ommx::{
+    artifact::media_types,
+    v1::{Function, State},
+    Evaluate, Message,
+};
+use pyo3::{prelude::*, types::PyBytes};
+use std::{
+    collections::{BTreeSet, HashMap},
+    path::PathBuf,
+};
 
 #[pyclass]
 #[pyo3(module = "ommx._ommx_rust")]
@@ -50,9 +57,20 @@ impl Artifact {
     }
 }
 
+#[pyfunction]
+pub fn evaluate_function<'py>(
+    function: &Bound<'py, PyBytes>,
+    state: &Bound<'py, PyBytes>,
+) -> Result<(f64, BTreeSet<u64>)> {
+    let state = State::decode(state.as_bytes())?;
+    let function = Function::decode(function.as_bytes())?;
+    function.evaluate(&state)
+}
+
 #[pymodule]
-fn _ommx_rust(_py: Python, m: Bound<PyModule>) -> PyResult<()> {
+fn _ommx_rust(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add_class::<Artifact>()?;
     m.add_class::<Descriptor>()?;
+    m.add_function(wrap_pyfunction!(evaluate_function, m)?)?;
     Ok(())
 }
