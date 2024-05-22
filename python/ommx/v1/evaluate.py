2084614ea84b697a39fd0991f2d619af3ec6329c
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-22 12:27:39 +0900

    Python API for evaluate

---
+++ python/ommx/v1/evaluate.py
@@ -0,0 +1,21 @@
+from typing import overload
+
+from .function_pb2 import Function
+from .solution_pb2 import State, Solution
+from .instance_pb2 import Instance
+
+from .._ommx_rust import evaluate_function
+
+@overload
+def evaluate(obj: Function, state: State) -> tuple[float, set[int]]: ...
+
+@overload
+def evaluate(obj: Instance, state: State) -> tuple[Solution, set[int]]: ...
+
+def evaluate(obj: Function | Instance, state: State) -> tuple[float | Solution, set[int]]:
+    state_bytes = state.SerializeToString()
+    if isinstance(obj, Function):
+        function_bytes = obj.SerializeToString()
+        return evaluate_function(function_bytes, state_bytes)
+
+    raise NotImplementedError(f"Evaluation for {type(obj)} is not implemented yet")
