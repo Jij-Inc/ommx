bbaa78cb3cdd85df868e271356e0f1edf3e6e91e
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-23 11:37:12 +0900

    Split artifact generation into a separate example

---
+++ rust/ommx/examples/artifact_archive.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+use colored::Colorize;
+use ommx::{artifact::Builder, random::random_lp};
+use rand::SeedableRng;
+use std::path::Path;
+
+fn main() -> Result<()> {
+    let mut rng = rand_xoshiro::Xoshiro256StarStar::seed_from_u64(0);
+    let lp = random_lp(&mut rng, 5, 7);
+
+    // "data" directory is at the root of the repository
+    let manifest_root: &Path = env!("CARGO_MANIFEST_DIR").as_ref();
+    let repo_root = manifest_root.join("../..").canonicalize()?;
+    let data_root = repo_root.join("data");
+
+    let out = data_root.join("random_lp_instance.ommx");
+    if out.exists() {
+        println!("{:>12} {}", "Removing".red().bold(), out.display());
+        std::fs::remove_file(&out)?;
+    }
+    println!("{:>12} {}", "New Artifact".blue().bold(), out.display());
+    let _artifact = Builder::new_archive_unnamed(out)?
+        .add_instance(lp, Default::default())?
+        .build()?;
+    Ok(())
+}
--- rust/ommx/src/artifact.rs
+++ rust/ommx/src/artifact.rs
@@ -157,26 +157,3 @@ impl<Base: Image> Artifact<Base> {
         Ok(out)
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::random::random_lp;
-    use rand::SeedableRng;
-    use std::path::Path;
-
-    #[test]
-    fn save_random_lp_as_archive() -> Result<()> {
-        let mut rng = rand_xoshiro::Xoshiro256StarStar::seed_from_u64(0);
-        let lp = random_lp(&mut rng, 5, 7);
-        let root: &Path = env!("CARGO_MANIFEST_DIR").as_ref();
-        let out = root.join("random_lp.ommx");
-        if out.exists() {
-            std::fs::remove_file(&out)?;
-        }
-        let _artifact = Builder::new_archive_unnamed(out)?
-            .add_instance(lp, Default::default())?
-            .build()?;
-        Ok(())
-    }
-}
