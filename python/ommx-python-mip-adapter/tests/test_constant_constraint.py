f6c554ea4e820a03ca8e71bbd3209f5c68df36cc
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-06-27 23:52:09 +0900

    constant constraints

---
+++ python/ommx-python-mip-adapter/tests/test_constant_constraint.py
@@ -0,0 +1,40 @@
+import ommx_python_mip_adapter as adapter
+from ommx.v1 import Instance, DecisionVariable, Linear
+
+
+def test_constant_constraint_feasible():
+    x = DecisionVariable.continuous(0)
+    instance = Instance.from_components(
+        decision_variables=[x],
+        objective=x,
+        constraints=[
+            # 1 >= 0 is always true
+            Linear(terms={}, constant=1) >= 0,
+            x <= 1,
+        ],
+        sense=Instance.MAXIMIZE,
+    )
+    result = adapter.solve(instance)
+    assert result.HasField("solution")
+
+    solution = result.solution
+    assert solution.state.entries == {0: 1.0}
+    assert solution.objective == 1.0
+
+    assert len(solution.evaluated_constraints) == 2
+
+
+def test_constant_constraint_infeasible():
+    x = DecisionVariable.continuous(0)
+    instance = Instance.from_components(
+        decision_variables=[x],
+        objective=x,
+        constraints=[
+            # -1 >= 0 is always false
+            Linear(terms={}, constant=-1) >= 0,
+            x <= 1,
+        ],
+        sense=Instance.MAXIMIZE,
+    )
+    result = adapter.solve(instance)
+    assert result.HasField("infeasible")
