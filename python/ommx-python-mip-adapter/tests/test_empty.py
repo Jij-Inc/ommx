e8b7f7bac63603d220172809ae0def778b251906
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-06-28 08:51:25 +0900

    Add test case

--- python/ommx-python-mip-adapter/tests/test_empty.py
+++ python/ommx-python-mip-adapter/tests/test_empty.py
@@ -4,8 +4,13 @@ from ommx.v1 import Instance, DecisionVariable
 
 def test_empty():
     x = DecisionVariable.binary(0)
+    y = DecisionVariable.integer(1, lower=0, upper=10)
+    z = DecisionVariable.binary(2)
     instance = Instance.from_components(
-        decision_variables=[x], objective=x, sense=Instance.MINIMIZE, constraints=[]
+        decision_variables=[x, y, z],
+        objective=x - y + z,
+        sense=Instance.MINIMIZE,
+        constraints=[x + z == 1],
     )
-    result = adapter.solve(instance)
+    result = adapter.solve(instance, relax=True)
     assert result.HasField("solution")
