8fb3fd8edb78999a13097fd998390bbaf2efed47
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-09-19 17:16:11 +0900

    Fix decision_variable field in random_lp_instance.ommx

--- rust/ommx/src/random.rs
+++ rust/ommx/src/random.rs
@@ -1,11 +1,27 @@
 //! Randomly generate OMMX components for benchmarking and testing
 
-use crate::v1::{self, linear::Term, Constraint, Equality};
+use crate::v1::{
+    self, decision_variable::Kind, linear::Term, Bound, Constraint, DecisionVariable, Equality,
+};
 use rand::Rng;
 
 /// Create a random linear programming (LP) instance in a form of `min c^T x` subject to `Ax = b` and `x >= 0` with continuous variables `x`.
 pub fn random_lp(rng: &mut impl Rng, num_variables: usize, num_constraints: usize) -> v1::Instance {
     let mut instance = v1::Instance::default();
+    instance.decision_variables = (0..num_variables)
+        .map(|i| {
+            let mut var = DecisionVariable::default();
+            var.id = i as u64;
+            var.kind = Kind::Continuous as i32;
+            var.name = Some("x".into());
+            var.subscripts = vec![i as i64];
+            var.bound = Some(Bound {
+                lower: 0.0,
+                upper: f64::INFINITY,
+            });
+            var
+        })
+        .collect();
     for constraint_id in 0..num_constraints {
         let mut linear = v1::Linear::default();
         for id in 0..num_variables {
