0d16d6f916261d6718b96f238b6526bc8f26901c
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-10-04 14:47:00 +0900

    impl Add for Linear

--- rust/ommx/src/convert/linear.rs
+++ rust/ommx/src/convert/linear.rs
@@ -1,5 +1,8 @@
 use crate::v1::{linear::Term, Linear};
-use std::collections::BTreeSet;
+use std::{
+    collections::{BTreeSet, HashMap},
+    ops::Add,
+};
 
 impl Linear {
     pub fn new(terms: impl Iterator<Item = (u64, f64)>, constant: f64) -> Self {
@@ -26,5 +29,51 @@ impl Linear {
 impl From<u64> for Linear {
     fn from(id: u64) -> Self {
         Self::single_term(id, 1.0)
+    }
+}
+
+impl From<f64> for Linear {
+    fn from(constant: f64) -> Self {
+        Self {
+            terms: vec![],
+            constant,
+        }
+    }
+}
+
+impl Add for Linear {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        let mut terms = HashMap::new();
+        for term in self.terms.iter().chain(rhs.terms.iter()) {
+            *terms.entry(term.id).or_default() += term.coefficient;
+        }
+        Self {
+            terms: terms
+                .into_iter()
+                .map(|(id, coefficient)| Term { id, coefficient })
+                .collect(),
+            constant: self.constant + rhs.constant,
+        }
+    }
+}
+
+impl Add<f64> for Linear {
+    type Output = Self;
+
+    fn add(self, rhs: f64) -> Self {
+        Self {
+            terms: self.terms,
+            constant: self.constant + rhs,
+        }
+    }
+}
+
+impl Add<Linear> for f64 {
+    type Output = Linear;
+
+    fn add(self, rhs: Linear) -> Linear {
+        rhs + self
     }
 }
