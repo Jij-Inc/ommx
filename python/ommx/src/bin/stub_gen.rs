6a31feef0bb5c6b9bce31067d1b502e210488819
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-10-10 21:28:35 +0900

    Automatic reformat

--- python/ommx/src/bin/stub_gen.rs
+++ python/ommx/src/bin/stub_gen.rs
@@ -1,7 +1,14 @@
 use pyo3_stub_gen::Result;
+use std::{path::Path, process::Command};
 
 fn main() -> Result<()> {
     let stub = _ommx_rust::stub_info()?;
     stub.generate()?;
+
+    let root: &Path = env!("CARGO_MANIFEST_DIR").as_ref();
+    Command::new("ruff")
+        .arg("format")
+        .arg(root.join("ommx/_ommx_rust.pyi"))
+        .status()?;
     Ok(())
 }
