243c3254aa95f1c4a3f09600dafc5d32c93ed69d
Zengor <ialmeida@edu.unifor.br>
2024-10-23 13:01:28 +0900

    Write rows

--- rust/ommx/src/mps/to_mps.rs
+++ rust/ommx/src/mps/to_mps.rs
@@ -4,24 +4,7 @@ use crate::{
 };
 use std::io::Write;
 
-pub fn to_mps(instance: v1::Instance) -> Mps {
-    let mut mps = Mps::default();
-    mps.name = instance
-        .description
-        .map(|descr| descr.name.clone())
-        .flatten()
-        .unwrap_or(String::from("Converted OMMX problem"));
-    // TODO fallible conversion?
-    mps.obj_sense = match instance.sense {
-        // v1::instance::Sense::Maximize
-        // TODO more robust way to write this?
-        2 => ObjSense::Max,
-        _ => ObjSense::Min,
-    };
-    todo!()
-}
-
-fn write_mps<W: std::io::Write>(mps: &Mps, out: &mut W) -> anyhow::Result<()> {
+fn write_mps<W: std::io::Write>(mps: &v1::Instance, out: &mut W) -> anyhow::Result<()> {
     write_beginning(mps, out)?;
     write_rows(mps, out)?;
     write_columns(mps, out)?;
@@ -31,21 +14,51 @@ fn write_mps<W: std::io::Write>(mps: &Mps, out: &mut W) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn write_beginning<W: std::io::Write>(mps: &Mps, out: &mut W) -> anyhow::Result<()> {
-    writeln!(out, "NAME {}", mps.name)?;
-    writeln!(out, "OBJSENSE {}", mps.obj_sense)?;
+fn write_beginning<W: std::io::Write>(instance: &v1::Instance, out: &mut W) -> anyhow::Result<()> {
+    let name = instance
+        .description
+        .clone()
+        .map(|descr| descr.name)
+        .flatten()
+        .unwrap_or(String::from("Converted OMMX problem"));
+    // TODO fallible conversion?
+    let obj_sense = match instance.sense {
+        // v1::instance::Sense::Maximize
+        // TODO more robust way to write this?
+        2 => ObjSense::Max,
+        _ => ObjSense::Min,
+    };
+    writeln!(out, "NAME {name}")?;
+    writeln!(out, "OBJSENSE {obj_sense}")?;
     Ok(())
 }
 
-fn write_rows<W: Write>(mps: &Mps, out: &mut W) -> anyhow::Result<()> {
+fn write_rows<W: Write>(instance: &v1::Instance, out: &mut W) -> anyhow::Result<()> {
+    writeln!(out, "ROWS")?;
+    // each line must be ` Kind  constr_name`, and include objective
+    writeln!(out, " N OBJ")?;
+    // ommx instances are always <= 0 or = 0, so `Kind` will always be either N or L.
+    for constr in instance.constraints.iter() {
+        let kind = match constr.equality {
+            // v1::Equality::LessThanEqualToZero
+            2 => "L",
+            // assuming EqualToZero when unspecified. Error instead?
+            _ => "N",
+        };
+        let name = constr
+            .name
+            .clone()
+            .unwrap_or_else(|| format!("constr_id{}", constr.id));
+        writeln!(out, " {kind} {name}")?;
+    }
+    Ok(())
+}
+fn write_columns<W: Write>(instance: &v1::Instance, out: &mut W) -> anyhow::Result<()> {
     todo!()
 }
-fn write_columns<W: Write>(mps: &Mps, out: &mut W) -> anyhow::Result<()> {
+fn write_rhs<W: Write>(instance: &v1::Instance, out: &mut W) -> anyhow::Result<()> {
     todo!()
 }
-fn write_rhs<W: Write>(mps: &Mps, out: &mut W) -> anyhow::Result<()> {
-    todo!()
-}
-fn write_bounds<W: Write>(mps: &Mps, out: &mut W) -> anyhow::Result<()> {
+fn write_bounds<W: Write>(instance: &v1::Instance, out: &mut W) -> anyhow::Result<()> {
     todo!()
 }
