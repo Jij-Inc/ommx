35551606a97001c566dcfabe3cec5635951b0228
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-11-20 12:43:33 +0900

    impl From<Instance> for ParametricInstance

--- rust/ommx/src/convert/parametric_instance.rs
+++ rust/ommx/src/convert/parametric_instance.rs
@@ -1,6 +1,28 @@
 use crate::v1::{Instance, Parameters, ParametricInstance};
 use anyhow::Result;
 
+impl From<Instance> for ParametricInstance {
+    fn from(
+        Instance {
+            description,
+            objective,
+            constraints,
+            decision_variables,
+            sense,
+            parameters: _, // Drop previous parameters
+        }: Instance,
+    ) -> Self {
+        Self {
+            description,
+            objective,
+            constraints,
+            decision_variables,
+            sense,
+            parameters: Default::default(),
+        }
+    }
+}
+
 impl ParametricInstance {
     /// Create a new [Instance] with the given parameters.
     pub fn with_parameters(&self, parameters: Parameters) -> Result<Instance> {
