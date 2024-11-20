51ca0214484f7367523df90cbb5dc2b45c37861b
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-11-20 20:46:46 +0900

    Add convert::state submod

--- rust/ommx/src/convert.rs
+++ rust/ommx/src/convert.rs
@@ -146,12 +146,4 @@ mod function;
 mod linear;
 mod polynomial;
 mod quadratic;
-
-use crate::v1::State;
-use std::collections::HashMap;
-
-impl From<HashMap<u64, f64>> for State {
-    fn from(entries: HashMap<u64, f64>) -> Self {
-        Self { entries }
-    }
-}
+mod state;
