d9da5e6ff861fd32778fec239a1c9d9cd228c920
NY57 <nishimuray.theory57@gmail.com>
2024-11-25 11:17:09 +0900

    modify  model_to_solution and add model_to_state

---
+++ python/ommx-pyscipopt-adapter/ommx_pyscipopt_adapter/__init__.py
@@ -0,0 +1,11 @@
+from .adapter import (
+    instance_to_model,
+    model_to_state,
+    model_to_solution,
+)
+
+__all__ = [
+    "instance_to_model",
+    "model_to_state",
+    "model_to_solution",
+]
