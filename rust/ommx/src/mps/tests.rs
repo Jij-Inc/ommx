71d640174eb197558dbc4f7923eb272ada96c902
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-11-27 21:40:06 +0900

    Test roundtrip for MPS format

--- rust/ommx/src/mps/tests.rs
+++ rust/ommx/src/mps/tests.rs
@@ -1,4 +1,5 @@
 use crate::{mps::*, v1::Instance};
+use approx::AbsDiffEq;
 use proptest::prelude::*;
 
 proptest! {
@@ -6,5 +7,14 @@ proptest! {
     fn test_write_mps(instance in Instance::arbitrary_lp()) {
         let mut buffer = Vec::new();
         prop_assert!(to_mps::write_mps(&instance, &mut buffer).is_ok())
+    }
+
+    #[test]
+    fn test_roundtrip(instance in Instance::arbitrary_lp()) {
+        let mut buffer = Vec::new();
+        prop_assert!(to_mps::write_mps(&instance, &mut buffer).is_ok());
+
+        let loaded_instance = load_reader(&buffer[..]).unwrap();
+        prop_assert!(instance.abs_diff_eq(&dbg!(loaded_instance), 1e-6))
     }
 }
