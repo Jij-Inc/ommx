Jij-Inc/ommx

commit a8514c5fbe91d9c4cce43630f0edc086a4045356

Modeler API for Rust SDK (#120)
- Impement `Add`, `Mul`, and more algebraic operators for `Linear`,
`Quadratic`, `Polynomial`, and `Function`
- Implement `From`, `FromIterator`, and `IntoIterator` for them
- Implement `Arbitrary` and `AbsDiffEq` for property-based testing

rust/ommx/src/convert/quadratic.rs
@@ -1,0 +1,276 @@
+use crate::v1::{Linear, Polynomial, Quadratic};
+use approx::AbsDiffEq;
+use num::Zero;
+use proptest::prelude::*;
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    ops::{Add, Mul},
+};
+
+impl Zero for Quadratic {
+    fn zero() -> Self {
+        Self {
+            columns: vec![],
+            rows: vec![],
+            values: vec![],
+            linear: Some(Linear::zero()),
+        }
+    }
+
+    fn is_zero(&self) -> bool {
+        self.columns.is_empty()
+            && self.rows.is_empty()
+            && self.values.is_empty()
+            && self.linear.as_ref().map_or(true, |l| l.is_zero())
+    }
+}
+
+impl Quadratic {
+    pub fn used_decision_variable_ids(&self) -> BTreeSet<u64> {
+        self.columns
+            .iter()
+            .chain(self.rows.iter())
+            .cloned()
+            .collect()
+    }
+
+    fn quad_iter(&self) -> impl Iterator<Item = ((u64, u64), f64)> + '_ {
+        self.columns
+            .iter()
+            .zip(self.rows.iter())
+            .zip(self.values.iter())
+            .map(|((column, row), value)| ((*column, *row), *value))
+    }
+}
+
+impl From<f64> for Quadratic {
+    fn from(c: f64) -> Self {
+        Self {
+            columns: Vec::new(),
+            rows: Vec::new(),
+            values: Vec::new(),
+            linear: Some(c.into()),
+        }
+    }
+}
+
+impl From<Linear> for Quadratic {
+    fn from(l: Linear) -> Self {
+        Self {
+            columns: Vec::new(),
+            rows: Vec::new(),
+            values: Vec::new(),
+            linear: Some(l),
+        }
+    }
+}
+
+impl FromIterator<((u64, u64), f64)> for Quadratic {
+    fn from_iter<I: IntoIterator<Item = ((u64, u64), f64)>>(iter: I) -> Self {
+        let mut terms = BTreeMap::new();
+        for ((row, col), value) in iter {
+            let id = if row < col { (row, col) } else { (col, row) };
+            *terms.entry(id).or_default() += value;
+        }
+        let mut columns = Vec::new();
+        let mut rows = Vec::new();
+        let mut values = Vec::new();
+        for ((row, col), value) in terms {
+            columns.push(col);
+            rows.push(row);
+            values.push(value);
+        }
+        Self {
+            columns,
+            rows,
+            values,
+            linear: None,
+        }
+    }
+}
+
+impl IntoIterator for Quadratic {
+    type Item = (Vec<u64>, f64);
+    type IntoIter = Box<dyn Iterator<Item = Self::Item>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        assert_eq!(self.columns.len(), self.rows.len());
+        assert_eq!(self.columns.len(), self.values.len());
+        let n = self.columns.len();
+        let quad = (0..n).map(move |i| (vec![self.columns[i], self.rows[i]], self.values[i]));
+        if let Some(linear) = self.linear {
+            Box::new(
+                quad.chain(
+                    linear
+                        .into_iter()
+                        .map(|(id, c)| (id.into_iter().collect(), c)),
+                ),
+            )
+        } else {
+            Box::new(quad)
+        }
+    }
+}
+
+impl Add for Quadratic {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        let mut map: BTreeMap<(u64, u64), f64> = self.quad_iter().collect();
+        for (id, value) in rhs.quad_iter() {
+            let v = map.entry(id).or_default();
+            *v += value;
+            if v.abs() <= f64::EPSILON {
+                map.remove(&id);
+            }
+        }
+        let mut out: Self = map.into_iter().collect();
+        out.linear = match (self.linear, rhs.linear) {
+            (Some(l), Some(r)) => {
+                let out = l + r;
+                if out.is_zero() {
+                    None
+                } else {
+                    Some(out)
+                }
+            }
+            (Some(l), None) | (None, Some(l)) => Some(l),
+            (None, None) => None,
+        };
+        out
+    }
+}
+
+impl Add<Linear> for Quadratic {
+    type Output = Self;
+
+    fn add(mut self, rhs: Linear) -> Self {
+        if let Some(linear) = self.linear {
+            self.linear = Some(linear + rhs);
+        } else {
+            self.linear = Some(rhs);
+        }
+        self
+    }
+}
+
+impl Add<f64> for Quadratic {
+    type Output = Self;
+
+    fn add(mut self, rhs: f64) -> Self {
+        if let Some(linear) = self.linear {
+            self.linear = Some(linear + rhs);
+        } else {
+            self.linear = Some(rhs.into());
+        }
+        self
+    }
+}
+
+impl_add_inverse!(Linear, Quadratic);
+impl_add_inverse!(f64, Quadratic);
+impl_sub_by_neg_add!(Quadratic, Linear);
+impl_sub_by_neg_add!(Quadratic, f64);
+impl_sub_by_neg_add!(Quadratic, Quadratic);
+
+impl Mul for Quadratic {
+    type Output = Polynomial;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut terms = BTreeMap::new();
+        for (id_l, value_l) in self.into_iter() {
+            for (mut id_r, value_r) in rhs.clone().into_iter() {
+                id_r.append(&mut id_l.clone());
+                id_r.sort_unstable();
+                *terms.entry(id_r).or_default() += value_l * value_r;
+            }
+        }
+        terms.into_iter().collect()
+    }
+}
+
+impl_mul_from!(Quadratic, Linear, Polynomial);
+impl_mul_inverse!(Linear, Quadratic);
+
+impl Mul<f64> for Quadratic {
+    type Output = Self;
+
+    fn mul(mut self, rhs: f64) -> Self {
+        if rhs.is_zero() {
+            return Self::zero();
+        }
+        for value in self.values.iter_mut() {
+            *value *= rhs;
+        }
+        if let Some(linear) = self.linear {
+            self.linear = Some(linear * rhs);
+        } // 0 * rhs = 0
+        self
+    }
+}
+
+impl_mul_inverse!(f64, Quadratic);
+impl_neg_by_mul!(Quadratic);
+
+impl Arbitrary for Quadratic {
+    type Parameters = ();
+    type Strategy = BoxedStrategy<Self>;
+
+    fn arbitrary_with(_: Self::Parameters) -> Self::Strategy {
+        let num_terms = 0..10_usize;
+        let terms = num_terms.prop_flat_map(|num_terms| {
+            proptest::collection::vec(
+                (
+                    (0..(2 * num_terms as u64), 0..(2 * num_terms as u64)),
+                    prop_oneof![Just(0.0), -1.0..1.0],
+                ),
+                num_terms,
+            )
+        });
+        let linear = Linear::arbitrary_with(());
+        (terms, linear)
+            .prop_map(|(terms, linear)| {
+                let mut quad: Quadratic = terms.into_iter().collect();
+                quad.linear = Some(linear);
+                quad
+            })
+            .boxed()
+    }
+}
+
+/// Compare coefficients in sup-norm.
+impl AbsDiffEq for Quadratic {
+    type Epsilon = f64;
+
+    fn default_epsilon() -> Self::Epsilon {
+        f64::default_epsilon()
+    }
+
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        match (&self.linear, &other.linear) {
+            (Some(l), Some(r)) => {
+                if !l.abs_diff_eq(r, epsilon) {
+                    return false;
+                }
+            }
+            (Some(l), None) | (None, Some(l)) => {
+                if !l.abs_diff_eq(&Linear::zero(), epsilon) {
+                    return false;
+                }
+            }
+            (None, None) => {}
+        }
+        let sub = self.clone() - other.clone();
+        for (_, value) in sub.into_iter() {
+            if !value.abs_diff_eq(&0.0, epsilon) {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    test_algebraic!(super::Quadratic);
+}

.gitignore
@@ -5,6 +5,9 @@
 # Rust
 target/
 
+# Proptest
+rust/ommx/proptest-regressions/
+
 # Byte-compiled / optimized / DLL files
 __pycache__/
 .pytest_cache/

Cargo.toml
@@ -13,6 +13,7 @@ license = "MIT OR Apache-2.0"
 
 [workspace.dependencies]
 anyhow = "1.0.89"
+approx = "0.5.1"
 base64 = "0.22.1"
 built = { version = "0.7.4", features = ["git2"] }
 chrono = "0.4.38"
@@ -27,6 +28,7 @@ itertools = "0.13.0"
 indoc = "2.0.5"
 log = "0.4.22"
 maplit = "1.0.2"
+num = "0.4.3"
 ocipkg = "0.3.9"
 proptest = "1.5.0"
 prost = "0.12.6"
@@ -40,3 +42,4 @@ serde-pyobject = "0.3.0"
 serde_json = "1.0.128"
 thiserror = "1.0.64"
 url = "2.5.2"
+uuid = { version = "1.10.0", features = ["v4"] }

rust/ommx/Cargo.toml
@@ -15,6 +15,7 @@ categories    = ["data-structures", "mathematics", "science"]
 
 [dependencies]
 anyhow.workspace = true
+approx.workspace = true
 base64.workspace = true
 chrono.workspace = true
 clap.workspace = true
@@ -26,6 +27,7 @@ flate2.workspace = true
 itertools.workspace = true
 log.workspace = true
 maplit.workspace = true
+num.workspace = true
 ocipkg.workspace = true
 proptest.workspace = true
 prost.workspace = true
@@ -35,7 +37,7 @@ serde.workspace = true
 serde_json.workspace = true
 thiserror.workspace = true
 url.workspace = true
-uuid = { version = "1.10.0", features = ["v4"] }
+uuid.workspace = true
 
 [dev-dependencies]
 colored.workspace = true

rust/ommx/src/convert.rs
@@ -1,82 +1,156 @@
 //! Additional trait implementations for generated codes
 
-use crate::v1::{
-    function::{self, Function as FunctionEnum},
-    linear::Term,
-    Function, Linear, Polynomial, Quadratic, State,
-};
-use std::collections::{BTreeSet, HashMap};
-
-impl From<function::Function> for Function {
-    fn from(f: function::Function) -> Self {
-        Self { function: Some(f) }
-    }
-}
-
-impl From<Linear> for Function {
-    fn from(linear: Linear) -> Self {
-        Self {
-            function: Some(function::Function::Linear(linear)),
+macro_rules! impl_add_inverse {
+    ($lhs:ty, $rhs:ty) => {
+        impl ::std::ops::Add<$rhs> for $lhs {
+            type Output = <$rhs as ::std::ops::Add<$lhs>>::Output;
+            fn add(self, rhs: $rhs) -> Self::Output {
+                rhs + self
+            }
         }
-    }
+    };
 }
 
-impl From<Quadratic> for Function {
-    fn from(q: Quadratic) -> Self {
-        Self {
-            function: Some(function::Function::Quadratic(q)),
+macro_rules! impl_add_from {
+    ($lhs:ty, $rhs:ty) => {
+        impl ::std::ops::Add<$rhs> for $lhs {
+            type Output = $lhs;
+            fn add(self, rhs: $rhs) -> Self::Output {
+                self + <$lhs>::from(rhs)
+            }
         }
-    }
+    };
 }
+
+macro_rules! impl_sub_by_neg_add {
+    ($lhs:ty, $rhs:ty) => {
+        impl ::std::ops::Sub<$rhs> for $lhs {
+            type Output = $lhs;
+            fn sub(self, rhs: $rhs) -> Self::Output {
+                self + (-rhs)
+            }
+        }
+    };
+}
+
+macro_rules! impl_mul_inverse {
+    ($lhs:ty, $rhs:ty) => {
+        impl ::std::ops::Mul<$rhs> for $lhs {
+            type Output = <$rhs as ::std::ops::Mul<$lhs>>::Output;
+            fn mul(self, rhs: $rhs) -> Self::Output {
+                rhs * self
+            }
+        }
+    };
+}
+
+macro_rules! impl_mul_from {
+    ($lhs:ty, $rhs:ty, $output:ty) => {
+        impl ::std::ops::Mul<$rhs> for $lhs {
+            type Output = $output;
+            fn mul(self, rhs: $rhs) -> Self::Output {
+                self * <$lhs>::from(rhs)
+            }
+        }
+    };
+}
+
+macro_rules! impl_neg_by_mul {
+    ($ty:ty) => {
+        impl ::std::ops::Neg for $ty {
+            type Output = $ty;
+            fn neg(self) -> Self::Output {
+                self * -1.0
+            }
+        }
+    };
+}
+
+#[cfg(test)]
+macro_rules! test_algebraic {
+    ($target:ty) => {
+        use num::Zero;
+        use approx::AbsDiffEq;
+        #[allow(unused_imports)]
+        use proptest::prelude::*;
+
+        proptest! {
+            #[test]
+            fn test_zero(a in any::<$target>()) {
+                let z = a.clone() - a;
+                prop_assert!(z.is_zero());
+            }
+
+            #[test]
+            fn test_scalar_distributive(x in any::<$target>(), y in any::<$target>(), a in -1.0..1.0_f64) {
+                let a_xy = a * (x.clone() + y.clone());
+                let ax_ay = a * x + a * y;
+                prop_assert!(a_xy.abs_diff_eq(&ax_ay, 1e-10));
+            }
+
+            #[test]
+            fn test_add_associativity(a in any::<$target>(), b in any::<$target>(), c in any::<$target>()) {
+                let ab = a.clone() + b.clone();
+                let ab_c = ab.clone() + c.clone();
+                let bc = b.clone() + c.clone();
+                let a_bc = a.clone() + bc.clone();
+                prop_assert!(ab_c.abs_diff_eq(&a_bc, 1e-10), r#"
+                    a = {a:?}
+                    b = {b:?}
+                    c = {c:?}
+                    a+b = {ab:?}
+                    b+c = {bc:?}
+                    (a+b)+c = {ab_c:?}
+                    a+(b+c) = {a_bc:?}
+                "#);
+            }
+
+            #[test]
+            fn test_mul_associativity(a in any::<$target>(), b in any::<$target>(), c in any::<$target>()) {
+                let ab = a.clone() * b.clone();
+                let ab_c = ab.clone() * c.clone();
+                let bc = b * c;
+                let a_bc = a * bc.clone();
+                prop_assert!(a_bc.abs_diff_eq(&ab_c, 1e-10), r#"
+                    a*b = {ab:?}
+                    b*c = {bc:?}
+                    (a*b)*c = {ab_c:?}
+                    a*(b*c) = {a_bc:?}
+                "#);
+            }
+
+            #[test]
+            fn test_distributive(a in any::<$target>(), b in any::<$target>(), c in any::<$target>()) {
+                let bc = b.clone() + c.clone();
+                let a_bc = a.clone() * bc.clone();
+                let ab = a.clone() * b.clone();
+                let ac = a.clone() * c.clone();
+                let ab_ac = ab.clone() + ac.clone();
+                prop_assert!(a_bc.abs_diff_eq(&ab_ac, 1e-10), r#"
+                    a = {a:?}
+                    b = {b:?}
+                    c = {c:?}
+                    b+c = {bc:?}
+                    a*b = {ab:?}
+                    a*c = {ac:?}
+                    a*(b+c) = {a_bc:?}
+                    a*b+a*c = {ab_ac:?}
+                "#);
+            }
+        }
+    };
+}
+
+mod function;
+mod linear;
+mod polynomial;
+mod quadratic;
+
+use crate::v1::State;
+use std::collections::HashMap;
 
 impl From<HashMap<u64, f64>> for State {
     fn from(entries: HashMap<u64, f64>) -> Self {
         Self { entries }
-    }
-}
-
-impl Function {
-    pub fn used_decision_variable_ids(&self) -> BTreeSet<u64> {
-        match &self.function {
-            Some(FunctionEnum::Linear(linear)) => linear.used_decision_variable_ids(),
-            Some(FunctionEnum::Quadratic(quadratic)) => quadratic.used_decision_variable_ids(),
-            Some(FunctionEnum::Polynomial(poly)) => poly.used_decision_variable_ids(),
-            _ => BTreeSet::new(),
-        }
-    }
-}
-
-impl Linear {
-    pub fn new(terms: impl Iterator<Item = (u64, f64)>, constant: f64) -> Self {
-        Self {
-            terms: terms
-                .map(|(id, coefficient)| Term { id, coefficient })
-                .collect(),
-            constant,
-        }
-    }
-
-    pub fn used_decision_variable_ids(&self) -> BTreeSet<u64> {
-        self.terms.iter().map(|term| term.id).collect()
-    }
-}
-
-impl Quadratic {
-    pub fn used_decision_variable_ids(&self) -> BTreeSet<u64> {
-        self.columns
-            .iter()
-            .chain(self.rows.iter())
-            .cloned()
-            .collect()
-    }
-}
-
-impl Polynomial {
-    pub fn used_decision_variable_ids(&self) -> BTreeSet<u64> {
-        self.terms
-            .iter()
-            .flat_map(|term| term.ids.iter())
-            .cloned()
-            .collect()
     }
 }

rust/ommx/src/convert/function.rs
@@ -1,0 +1,277 @@
+use crate::v1::{
+    function::{self, Function as FunctionEnum},
+    Function, Linear, Polynomial, Quadratic,
+};
+use approx::AbsDiffEq;
+use num::Zero;
+use proptest::prelude::*;
+use std::{collections::BTreeSet, iter::*, ops::*};
+
+impl Zero for Function {
+    fn zero() -> Self {
+        Self {
+            function: Some(function::Function::Constant(0.0)),
+        }
+    }
+
+    fn is_zero(&self) -> bool {
+        match &self.function {
+            Some(FunctionEnum::Constant(c)) => c.is_zero(),
+            Some(FunctionEnum::Linear(linear)) => linear.is_zero(),
+            Some(FunctionEnum::Quadratic(quadratic)) => quadratic.is_zero(),
+            Some(FunctionEnum::Polynomial(poly)) => poly.is_zero(),
+            _ => false,
+        }
+    }
+}
+
+impl From<function::Function> for Function {
+    fn from(f: function::Function) -> Self {
+        Self { function: Some(f) }
+    }
+}
+
+impl From<Linear> for Function {
+    fn from(linear: Linear) -> Self {
+        Self {
+            function: Some(function::Function::Linear(linear)),
+        }
+    }
+}
+
+impl From<Quadratic> for Function {
+    fn from(q: Quadratic) -> Self {
+        Self {
+            function: Some(function::Function::Quadratic(q)),
+        }
+    }
+}
+
+impl From<Polynomial> for Function {
+    fn from(poly: Polynomial) -> Self {
+        Self {
+            function: Some(function::Function::Polynomial(poly)),
+        }
+    }
+}
+
+impl From<f64> for Function {
+    fn from(f: f64) -> Self {
+        Self {
+            function: Some(function::Function::Constant(f)),
+        }
+    }
+}
+
+impl FromIterator<(u64, f64)> for Function {
+    fn from_iter<I: IntoIterator<Item = (u64, f64)>>(iter: I) -> Self {
+        let linear: Linear = iter.into_iter().collect();
+        linear.into()
+    }
+}
+
+impl FromIterator<((u64, u64), f64)> for Function {
+    fn from_iter<I: IntoIterator<Item = ((u64, u64), f64)>>(iter: I) -> Self {
+        let quad: Quadratic = iter.into_iter().collect();
+        quad.into()
+    }
+}
+
+impl FromIterator<(Vec<u64>, f64)> for Function {
+    fn from_iter<I: IntoIterator<Item = (Vec<u64>, f64)>>(iter: I) -> Self {
+        let poly: Polynomial = iter.into_iter().collect();
+        poly.into()
+    }
+}
+
+impl Function {
+    pub fn used_decision_variable_ids(&self) -> BTreeSet<u64> {
+        match &self.function {
+            Some(FunctionEnum::Linear(linear)) => linear.used_decision_variable_ids(),
+            Some(FunctionEnum::Quadratic(quadratic)) => quadratic.used_decision_variable_ids(),
+            Some(FunctionEnum::Polynomial(poly)) => poly.used_decision_variable_ids(),
+            _ => BTreeSet::new(),
+        }
+    }
+}
+
+impl Add for Function {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        let lhs = self.function.expect("Empty Function");
+        let rhs = rhs.function.expect("Empty Function");
+        match (lhs, rhs) {
+            (FunctionEnum::Constant(lhs), FunctionEnum::Constant(rhs)) => Function::from(lhs + rhs),
+            // Linear output
+            (FunctionEnum::Linear(lhs), FunctionEnum::Constant(rhs))
+            | (FunctionEnum::Constant(rhs), FunctionEnum::Linear(lhs)) => Function::from(lhs + rhs),
+            (FunctionEnum::Linear(lhs), FunctionEnum::Linear(rhs)) => Function::from(lhs + rhs),
+            // Quadratic output
+            (FunctionEnum::Quadratic(lhs), FunctionEnum::Constant(rhs))
+            | (FunctionEnum::Constant(rhs), FunctionEnum::Quadratic(lhs)) => {
+                Function::from(lhs + rhs)
+            }
+            (FunctionEnum::Quadratic(lhs), FunctionEnum::Linear(rhs))
+            | (FunctionEnum::Linear(rhs), FunctionEnum::Quadratic(lhs)) => {
+                Function::from(lhs + rhs)
+            }
+            (FunctionEnum::Quadratic(lhs), FunctionEnum::Quadratic(rhs)) => {
+                Function::from(lhs + rhs)
+            }
+            // Polynomial output
+            (FunctionEnum::Polynomial(lhs), FunctionEnum::Constant(rhs))
+            | (FunctionEnum::Constant(rhs), FunctionEnum::Polynomial(lhs)) => {
+                Function::from(lhs + rhs)
+            }
+            (FunctionEnum::Polynomial(lhs), FunctionEnum::Linear(rhs))
+            | (FunctionEnum::Linear(rhs), FunctionEnum::Polynomial(lhs)) => {
+                Function::from(lhs + rhs)
+            }
+            (FunctionEnum::Polynomial(lhs), FunctionEnum::Quadratic(rhs))
+            | (FunctionEnum::Quadratic(rhs), FunctionEnum::Polynomial(lhs)) => {
+                Function::from(lhs + rhs)
+            }
+            (FunctionEnum::Polynomial(lhs), FunctionEnum::Polynomial(rhs)) => {
+                Function::from(lhs + rhs)
+            }
+        }
+    }
+}
+
+impl Mul for Function {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        let lhs = self.function.expect("Empty Function");
+        let rhs = rhs.function.expect("Empty Function");
+        match (lhs, rhs) {
+            (FunctionEnum::Constant(lhs), FunctionEnum::Constant(rhs)) => Function::from(lhs * rhs),
+            (FunctionEnum::Linear(lhs), FunctionEnum::Constant(rhs))
+            | (FunctionEnum::Constant(rhs), FunctionEnum::Linear(lhs)) => Function::from(lhs * rhs),
+            (FunctionEnum::Linear(lhs), FunctionEnum::Linear(rhs)) => Function::from(lhs * rhs),
+            (FunctionEnum::Quadratic(lhs), FunctionEnum::Constant(rhs))
+            | (FunctionEnum::Constant(rhs), FunctionEnum::Quadratic(lhs)) => {
+                Function::from(lhs * rhs)
+            }
+            (FunctionEnum::Quadratic(lhs), FunctionEnum::Linear(rhs))
+            | (FunctionEnum::Linear(rhs), FunctionEnum::Quadratic(lhs)) => {
+                Function::from(lhs * rhs)
+            }
+            (FunctionEnum::Quadratic(lhs), FunctionEnum::Quadratic(rhs)) => {
+                Function::from(lhs * rhs)
+            }
+            (FunctionEnum::Polynomial(lhs), FunctionEnum::Constant(rhs))
+            | (FunctionEnum::Constant(rhs), FunctionEnum::Polynomial(lhs)) => {
+                Function::from(lhs * rhs)
+            }
+            (FunctionEnum::Polynomial(lhs), FunctionEnum::Linear(rhs))
+            | (FunctionEnum::Linear(rhs), FunctionEnum::Polynomial(lhs)) => {
+                Function::from(lhs * rhs)
+            }
+            (FunctionEnum::Polynomial(lhs), FunctionEnum::Quadratic(rhs))
+            | (FunctionEnum::Quadratic(rhs), FunctionEnum::Polynomial(lhs)) => {
+                Function::from(lhs * rhs)
+            }
+            (FunctionEnum::Polynomial(lhs), FunctionEnum::Polynomial(rhs)) => {
+                Function::from(lhs * rhs)
+            }
+        }
+    }
+}
+
+impl_mul_from!(Function, f64, Function);
+impl_mul_inverse!(f64, Function);
+impl_neg_by_mul!(Function);
+impl_sub_by_neg_add!(Function, Function);
+
+impl Sum for Function {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Function::from(0.0), |acc, x| acc + x)
+    }
+}
+
+impl Product for Function {
+    fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Function::from(1.0), |acc, x| acc * x)
+    }
+}
+
+impl Arbitrary for Function {
+    type Parameters = ();
+    type Strategy = BoxedStrategy<Self>;
+
+    fn arbitrary_with(_: Self::Parameters) -> Self::Strategy {
+        prop_oneof![
+            prop_oneof![Just(0.0), -1.0..1.0_f64].prop_map(Function::from),
+            any::<Linear>().prop_map(Function::from),
+            any::<Quadratic>().prop_map(Function::from),
+            any::<Polynomial>().prop_map(Function::from),
+        ]
+        .boxed()
+    }
+}
+
+impl AbsDiffEq for Function {
+    type Epsilon = f64;
+
+    fn default_epsilon() -> Self::Epsilon {
+        f64::default_epsilon()
+    }
+
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        let lhs = self.function.as_ref().expect("Empty Function");
+        let rhs = other.function.as_ref().expect("Empty Function");
+        match (lhs, rhs) {
+            // Same order
+            (FunctionEnum::Constant(lhs), FunctionEnum::Constant(rhs)) => {
+                lhs.abs_diff_eq(rhs, epsilon)
+            }
+            (FunctionEnum::Linear(lhs), FunctionEnum::Linear(rhs)) => lhs.abs_diff_eq(rhs, epsilon),
+            (FunctionEnum::Quadratic(lhs), FunctionEnum::Quadratic(rhs)) => {
+                lhs.abs_diff_eq(rhs, epsilon)
+            }
+            (FunctionEnum::Polynomial(lhs), FunctionEnum::Polynomial(rhs)) => {
+                lhs.abs_diff_eq(rhs, epsilon)
+            }
+            // Upcast to higher order
+            (FunctionEnum::Constant(lhs), FunctionEnum::Linear(rhs))
+            | (FunctionEnum::Linear(rhs), FunctionEnum::Constant(lhs)) => {
+                let lhs = Linear::from(*lhs);
+                lhs.abs_diff_eq(rhs, epsilon)
+            }
+            (FunctionEnum::Constant(lhs), FunctionEnum::Quadratic(rhs))
+            | (FunctionEnum::Quadratic(rhs), FunctionEnum::Constant(lhs)) => {
+                let lhs = Quadratic::from(*lhs);
+                lhs.abs_diff_eq(rhs, epsilon)
+            }
+            (FunctionEnum::Constant(lhs), FunctionEnum::Polynomial(rhs))
+            | (FunctionEnum::Polynomial(rhs), FunctionEnum::Constant(lhs)) => {
+                let lhs = Polynomial::from(*lhs);
+                lhs.abs_diff_eq(rhs, epsilon)
+            }
+            (FunctionEnum::Linear(lhs), FunctionEnum::Quadratic(rhs))
+            | (FunctionEnum::Quadratic(rhs), FunctionEnum::Linear(lhs)) => {
+                let lhs = Quadratic::from(lhs.clone());
+                lhs.abs_diff_eq(rhs, epsilon)
+            }
+            (FunctionEnum::Linear(lhs), FunctionEnum::Polynomial(rhs))
+            | (FunctionEnum::Polynomial(rhs), FunctionEnum::Linear(lhs)) => {
+                let lhs = Polynomial::from(lhs.clone());
+                lhs.abs_diff_eq(rhs, epsilon)
+            }
+            (FunctionEnum::Quadratic(lhs), FunctionEnum::Polynomial(rhs))
+            | (FunctionEnum::Polynomial(rhs), FunctionEnum::Quadratic(lhs)) => {
+                let lhs = Polynomial::from(lhs.clone());
+                lhs.abs_diff_eq(rhs, epsilon)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    test_algebraic!(Function);
+}

commit 36a37c6951a348159bba595b058047234102ee45

`Quadratic::used_decision_variable_ids` not include IDs of `Linear` part (#176)
Found in #174 

This feature has been broken ever since it was introduced in #120.

rust/ommx/src/convert/quadratic.rs
@@ -30,10 +30,11 @@ impl Zero for Quadratic {
 
 impl Quadratic {
     pub fn used_decision_variable_ids(&self) -> BTreeSet<u64> {
-        self.columns
-            .iter()
-            .chain(self.rows.iter())
-            .cloned()
+        self.linear
+            .as_ref()
+            .map_or_else(BTreeSet::new, |l| l.used_decision_variable_ids())
+            .into_iter()
+            .chain(self.columns.iter().chain(self.rows.iter()).cloned())
             .collect()
     }
 