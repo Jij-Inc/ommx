a8ccac203c72b6d2fd8e0769759cae95c1bc29ce
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-11-28 20:03:10 +0900

    Arbitrary for Parameter

--- rust/ommx/src/convert/parameter.rs
+++ rust/ommx/src/convert/parameter.rs
@@ -1,5 +1,9 @@
 use crate::v1::{Function, Linear, Parameter, Polynomial, Quadratic};
-use std::ops::*;
+use proptest::prelude::*;
+use std::{
+    collections::{BTreeSet, HashMap},
+    ops::*,
+};
 
 impl From<&Parameter> for Linear {
     fn from(dv: &Parameter) -> Self {
@@ -85,4 +89,51 @@ impl Neg for &Parameter {
     fn neg(self) -> Self::Output {
         -Linear::from(self)
     }
+}
+
+impl Arbitrary for Parameter {
+    type Parameters = u64;
+    type Strategy = BoxedStrategy<Self>;
+
+    fn arbitrary_with(max_id: Self::Parameters) -> Self::Strategy {
+        let subscripts = prop_oneof![
+            Just(Vec::<i64>::new()),
+            proptest::collection::vec(-(max_id as i64)..=(max_id as i64), 1..=3),
+        ];
+        let parameters = prop_oneof![
+            Just(HashMap::<String, String>::new()),
+            proptest::collection::hash_map(String::arbitrary(), String::arbitrary(), 1..=3),
+        ];
+        (
+            0..=max_id,
+            Option::<String>::arbitrary(),
+            subscripts,
+            parameters,
+            Option::<String>::arbitrary(),
+        )
+            .prop_map(
+                |(id, name, subscripts, parameters, description)| Parameter {
+                    id,
+                    name,
+                    subscripts,
+                    parameters,
+                    description,
+                },
+            )
+            .boxed()
+    }
+}
+
+pub fn arbitrary_parameters(ids: BTreeSet<u64>) -> BoxedStrategy<Vec<Parameter>> {
+    (
+        proptest::collection::vec(Parameter::arbitrary(), ids.len()),
+        Just(ids),
+    )
+        .prop_map(|(mut dvs, used_ids)| {
+            for (dv, id) in dvs.iter_mut().zip(used_ids.iter()) {
+                dv.id = *id;
+            }
+            dvs
+        })
+        .boxed()
 }
