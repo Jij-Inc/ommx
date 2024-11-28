5ad14fc6649a24d53aff9b3a499c1a00cfd7f58f
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-11-28 18:21:42 +0900

    More APIs

--- rust/ommx/src/convert/decision_variable.rs
+++ rust/ommx/src/convert/decision_variable.rs
@@ -28,17 +28,68 @@ impl_from_decision_variable!(Function);
 
 impl Add for &DecisionVariable {
     type Output = Linear;
-
     fn add(self, rhs: Self) -> Self::Output {
         Linear::from(self) + Linear::from(rhs)
     }
 }
+
+macro_rules! impl_add_decision_variable {
+    ($t:ty) => {
+        impl Add<$t> for &DecisionVariable {
+            type Output = <Linear as Add<$t>>::Output;
+            fn add(self, rhs: $t) -> Self::Output {
+                Linear::from(self) + rhs
+            }
+        }
+        impl Add<&DecisionVariable> for $t {
+            type Output = <Linear as Add<$t>>::Output;
+            fn add(self, rhs: &DecisionVariable) -> Self::Output {
+                self + Linear::from(rhs)
+            }
+        }
+    };
+}
+impl_add_decision_variable!(f64);
+impl_add_decision_variable!(Linear);
+impl_add_decision_variable!(Quadratic);
+impl_add_decision_variable!(Polynomial);
+impl_add_decision_variable!(Function);
 
 impl Mul for &DecisionVariable {
     type Output = Quadratic;
 
     fn mul(self, rhs: Self) -> Self::Output {
         Linear::from(self) * Linear::from(rhs)
+    }
+}
+
+macro_rules! impl_mul_decision_variable {
+    ($t:ty) => {
+        impl Mul<$t> for &DecisionVariable {
+            type Output = <Linear as Mul<$t>>::Output;
+            fn mul(self, rhs: $t) -> Self::Output {
+                Linear::from(self) * rhs
+            }
+        }
+        impl Mul<&DecisionVariable> for $t {
+            type Output = <Linear as Mul<$t>>::Output;
+            fn mul(self, rhs: &DecisionVariable) -> Self::Output {
+                self * Linear::from(rhs)
+            }
+        }
+    };
+}
+impl_mul_decision_variable!(f64);
+impl_mul_decision_variable!(Linear);
+impl_mul_decision_variable!(Quadratic);
+impl_mul_decision_variable!(Polynomial);
+impl_mul_decision_variable!(Function);
+
+impl Neg for &DecisionVariable {
+    type Output = Linear;
+
+    fn neg(self) -> Self::Output {
+        -Linear::from(self)
     }
 }
 
