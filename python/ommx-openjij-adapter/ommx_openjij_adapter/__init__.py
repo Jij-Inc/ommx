ff74fb044915275c8a2ab0207e59be2b18dc9513
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-12-24 15:06:49 +0900

    Add more parameters to `sample_qubo_sa`

--- python/ommx-openjij-adapter/ommx_openjij_adapter/__init__.py
+++ python/ommx-openjij-adapter/ommx_openjij_adapter/__init__.py
@@ -1,6 +1,5 @@
 from ommx.v1 import Instance, State, Samples
 import openjij as oj
-from typing import Optional
 
 
 def response_to_samples(response: oj.Response) -> Samples:
@@ -27,12 +26,23 @@ def response_to_samples(response: oj.Response) -> Samples:
 
 
 def sample_qubo_sa(
-    instance: Instance, *, num_reads: int = 1, seed: Optional[int] = None
+    instance: Instance,
+    *,
+    beta_min: float | None = None,
+    beta_max: float | None = None,
+    num_sweeps: int | None = None,
+    num_reads: int | None = None,
+    schedule: list | None = None,
+    initial_state: list | dict | None = None,
+    updater: str | None = None,
+    sparse: bool | None = None,
+    reinitialize_state: bool | None = None,
+    seed: int | None = None,
 ) -> Samples:
     """
     Sampling QUBO with Simulated Annealing (SA) by [`openjij.SASampler`](https://openjij.github.io/OpenJij/reference/openjij/index.html#openjij.SASampler)
 
-    Note that input `instance` must be a QUBO (Quadratic Unconstrained Binary Optimization) problem, i.e.
+    The input `instance` must be a QUBO (Quadratic Unconstrained Binary Optimization) problem, i.e.
 
     - Every decision variables are binary
     - No constraint
@@ -40,8 +50,32 @@ def sample_qubo_sa(
     - Minimization problem
 
     You can convert a problem to QUBO via [`ommx.v1.Instance.penalty_method`](https://jij-inc.github.io/ommx/python/ommx/autoapi/ommx/v1/index.html#ommx.v1.Instance.penalty_method) or other corresponding method.
+
+    :param instance: ommx.v1.Instance representing a QUBO problem
+    :param beta_min: minimal value of inverse temperature
+    :param beta_max: maximum value of inverse temperature
+    :param num_sweeps: number of sweeps
+    :param num_reads: number of reads
+    :param schedule: list of inverse temperature
+    :param initial_state: initial state
+    :param updater: updater algorithm
+    :param sparse: use sparse matrix or not.
+    :param reinitialize_state: if true reinitialize state for each run
+    :param seed: seed for Monte Carlo algorithm
     """
     q = instance.as_qubo_format()
     sampler = oj.SASampler()
-    response = sampler.sample_qubo(q, num_reads=num_reads, seed=seed)  # type: ignore
+    response = sampler.sample_qubo(
+        q,  # type: ignore
+        beta_min=beta_min,
+        beta_max=beta_max,
+        num_sweeps=num_sweeps,
+        num_reads=num_reads,
+        schedule=schedule,
+        initial_state=initial_state,
+        updater=updater,
+        sparse=sparse,
+        reinitialize_state=reinitialize_state,
+        seed=seed,
+    )
     return response_to_samples(response)
