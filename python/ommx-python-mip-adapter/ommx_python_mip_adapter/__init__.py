1ffaf467aab68358e60a850cc9a9943b75c88f52
Zengor <ialmeida@edu.unifor.br>
2025-02-03 16:53:33 +0900

    Remove OMMXInstanceBuilder from public API

--- python/ommx-python-mip-adapter/ommx_python_mip_adapter/__init__.py
+++ python/ommx-python-mip-adapter/ommx_python_mip_adapter/__init__.py
@@ -1,11 +1,9 @@
 from .python_mip_to_ommx import (
-    OMMXInstanceBuilder,
     model_to_instance,
 )
 from .adapter import OMMXPythonMIPAdapter
 
 __all__ = [
     "model_to_instance",
-    "OMMXInstanceBuilder",
     "OMMXPythonMIPAdapter",
 ]
