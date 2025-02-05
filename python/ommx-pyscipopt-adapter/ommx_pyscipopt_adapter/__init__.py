ce442ac775186649b4ade767de85bac3de56efe4
Zengor <ialmeida@edu.unifor.br>
2025-02-05 14:32:38 +0900

    Remove ommx_to_psycipopt.py

--- python/ommx-pyscipopt-adapter/ommx_pyscipopt_adapter/__init__.py
+++ python/ommx-pyscipopt-adapter/ommx_pyscipopt_adapter/__init__.py
@@ -1,15 +1,7 @@
 from .exception import OMMXPySCIPOptAdapterError
-from .ommx_to_pyscipopt import (
-    instance_to_model,
-    model_to_state,
-    model_to_solution,
-)
 from .adapter import OMMXPySCIPOptAdapter
 
 __all__ = [
-    "instance_to_model",
-    "model_to_state",
-    "model_to_solution",
     "OMMXPySCIPOptAdapter",
     "OMMXPySCIPOptAdapterError",
 ]
