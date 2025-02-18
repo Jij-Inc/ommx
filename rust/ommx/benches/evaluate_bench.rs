864f8177f1b2f1f42cb2236df476a1447f3b798b
Hiromi Ishii <h.ishii@j-ij.com>
2025-02-18 15:56:18 +0900

    call partial_eval instead

--- rust/ommx/benches/evaluate_bench.rs
+++ rust/ommx/benches/evaluate_bench.rs
@@ -5,22 +5,26 @@ use ommx::{
 };
 
 fn bench_linear(c: &mut Criterion) {
-    let mut group = c.benchmark_group("sum");
+    let mut group = c.benchmark_group("sum-opaque");
     for n in 1..=100 {
-        group.bench_with_input(BenchmarkId::new("sum", n), &n, |b, n| {
-            b.iter(|| {
-                evaluate_with(
-                    black_box(&State::default()),
-                    black_box(&Linear::new((0..*n).map(|i| (i, 1.0)), 0.0)),
-                );
-            })
-        });
+        group.bench_with_input(
+            BenchmarkId::new("sum-opaque", format!("{n:03}")),
+            &n,
+            |b, n| {
+                b.iter(|| {
+                    partial_eval_with(
+                        black_box(&State::default()),
+                        black_box(&mut Linear::new((0..*n).map(|i| (i, 1.0)), 0.0)),
+                    );
+                })
+            },
+        );
     }
     group.finish();
 }
 
-fn evaluate_with<E: Evaluate>(state: &State, item: &E) {
-    let _ = item.evaluate(state);
+fn partial_eval_with<E: Evaluate>(state: &State, item: &mut E) {
+    let _ = item.partial_evaluate(state);
 }
 
 criterion_group!(eval_benches, bench_linear);
