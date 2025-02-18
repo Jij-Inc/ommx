ac46ae778f17d18e0e522283c4c9f2dfd9036ac8
Hiromi Ishii <h.ishii@j-ij.com>
2025-02-18 16:06:27 +0900

    complete

--- rust/ommx/benches/evaluate_bench.rs
+++ rust/ommx/benches/evaluate_bench.rs
@@ -5,10 +5,10 @@ use ommx::{
 };
 
 fn bench_linear(c: &mut Criterion) {
-    let mut group = c.benchmark_group("sum-opaque");
+    let mut sum_partial = c.benchmark_group("sum-partial");
     for n in 1..=100 {
-        group.bench_with_input(
-            BenchmarkId::new("sum-opaque", format!("{n:03}")),
+        sum_partial.bench_with_input(
+            BenchmarkId::new("sum-partial", format!("{n:03}")),
             &n,
             |b, n| {
                 b.iter(|| {
@@ -20,7 +20,28 @@ fn bench_linear(c: &mut Criterion) {
             },
         );
     }
-    group.finish();
+    sum_partial.finish();
+
+    let mut sum_total = c.benchmark_group("sum-total");
+    for n in 0..=100 {
+        sum_total.bench_with_input(
+            BenchmarkId::new("sum-total", format!("{n:03}")),
+            &n,
+            |b, n| {
+                b.iter(|| {
+                    partial_eval_with(
+                        black_box(&{
+                            let mut state = State::default();
+                            state.entries = (0..*n).map(|i| (i, i as f64)).collect();
+                            state
+                        }),
+                        black_box(&mut Linear::new((0..*n).map(|i| (i, 1.0)), 0.0)),
+                    );
+                })
+            },
+        );
+    }
+    sum_total.finish();
 }
 
 fn partial_eval_with<E: Evaluate>(state: &State, item: &mut E) {
