7e056d9d901847c357618f9d407dd432a09721c6
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-02-25 18:22:02 +0900

    Test for Quadratic::arbitrary_with

--- rust/ommx/src/random/quadratic.rs
+++ rust/ommx/src/random/quadratic.rs
@@ -1,4 +1,4 @@
-use super::{arbitrary_coefficient, num_terms_and_max_id, LinearParameters};
+use super::{arbitrary_coefficient_nonzero, num_terms_and_max_id, LinearParameters};
 use crate::v1::{Linear, Quadratic};
 use proptest::prelude::*;
 
@@ -24,16 +24,22 @@ impl Arbitrary for Quadratic {
     fn arbitrary_with(
         QuadraticParameters { num_terms, max_id }: Self::Parameters,
     ) -> Self::Strategy {
-        let terms = proptest::collection::vec(
-            ((0..=max_id, 0..=max_id), arbitrary_coefficient()),
-            num_terms,
-        );
-        let linear = Linear::arbitrary_with(LinearParameters { num_terms, max_id });
-        (terms, linear)
-            .prop_map(|(terms, linear)| {
-                let mut quad: Quadratic = terms.into_iter().collect();
-                quad.linear = Some(linear);
-                quad
+        (0..=num_terms)
+            .prop_flat_map(move |num_quad| {
+                let num_linear = num_terms - num_quad;
+                let terms = proptest::collection::vec(
+                    ((0..=max_id, 0..=max_id), arbitrary_coefficient_nonzero()),
+                    num_quad,
+                );
+                let linear = Linear::arbitrary_with(LinearParameters {
+                    num_terms: num_linear,
+                    max_id,
+                });
+                (terms, linear).prop_map(|(terms, linear)| {
+                    let mut quad: Quadratic = terms.into_iter().collect();
+                    quad.linear = Some(linear);
+                    quad
+                })
             })
             .boxed()
     }
@@ -45,5 +51,24 @@ impl Arbitrary for Quadratic {
                 Self::arbitrary_with(QuadraticParameters { num_terms, max_id })
             })
             .boxed()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    proptest! {
+        #[test]
+        fn test_arbitrary_quadratic(q in Quadratic::arbitrary_with(QuadraticParameters { num_terms: 5, max_id: 10 })) {
+            let mut count = 0;
+            for (ids, _) in q.into_iter() {
+                for &id in ids.iter() {
+                    prop_assert!(id <= 10);
+                }
+                count += 1;
+            }
+            prop_assert_eq!(count, 5);
+        }
     }
 }
