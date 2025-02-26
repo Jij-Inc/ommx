691e3342d88ab665050cf9374f0daf0cfda639bf
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-02-27 00:20:15 +0900

    Revise the arbitrary generation of polynomials to generate the largest degree terms first

--- rust/ommx/src/random/polynomial.rs
+++ rust/ommx/src/random/polynomial.rs
@@ -1,4 +1,5 @@
 use crate::v1::Polynomial;
+use num::Zero;
 use proptest::prelude::*;
 
 use super::{arbitrary_coefficient_nonzero, multi_choose, unique_sorted_ids};
@@ -15,6 +16,20 @@ impl PolynomialParameters {
         (0..=self.max_degree)
             .map(|d| multi_choose(self.max_id + 1, d as usize) as usize)
             .sum()
+    }
+
+    fn largest_degree_term_range(&self) -> std::ops::RangeInclusive<usize> {
+        let sub_max_terms = (0..self.max_degree)
+            .map(|d| multi_choose(self.max_id + 1, d as usize) as usize)
+            .sum::<usize>();
+        let largest_max_terms = multi_choose(self.max_id + 1, self.max_degree as usize) as usize;
+        let max = std::cmp::min(self.num_terms, largest_max_terms);
+        let min = if self.num_terms >= sub_max_terms {
+            self.num_terms - sub_max_terms
+        } else {
+            0
+        };
+        min..=max
     }
 
     pub fn smaller(&self) -> impl Strategy<Value = Self> {
@@ -51,24 +66,45 @@ impl Arbitrary for Polynomial {
     type Parameters = PolynomialParameters;
     type Strategy = BoxedStrategy<Self>;
 
-    fn arbitrary_with(
-        PolynomialParameters {
-            num_terms,
-            max_degree,
-            max_id,
-        }: Self::Parameters,
-    ) -> Self::Strategy {
-        let max_num_terms = (0..=max_degree)
-            .map(|d| multi_choose(max_id + 1, d as usize) as usize)
-            .sum();
+    fn arbitrary_with(p: Self::Parameters) -> Self::Strategy {
         assert!(
-            num_terms <= max_num_terms,
-            "num_terms({num_terms}) must be less than or equal to the possible maximum number of terms({max_num_terms})"
+            p.num_terms <= p.possible_max_terms(),
+            "num_terms({num_terms}) must be less than or equal to the possible maximum number of terms({max_num_terms})",
+            num_terms = p.num_terms,
+            max_num_terms = p.possible_max_terms()
         );
-        let ids = unique_sorted_ids(max_id, max_degree as usize, num_terms);
-        let coefficients = proptest::collection::vec(arbitrary_coefficient_nonzero(), num_terms);
-        (ids, coefficients)
-            .prop_map(|(ids, coefficients)| ids.into_iter().zip(coefficients).collect())
+        if p.max_degree == 0 {
+            if p.num_terms == 0 {
+                return Just(Polynomial::zero()).boxed();
+            }
+            return arbitrary_coefficient_nonzero()
+                .prop_map(|c| Polynomial::from(c))
+                .boxed();
+        }
+        p.largest_degree_term_range()
+            .prop_flat_map(move |num_largest| {
+                // The largest degree terms
+                let ids = unique_sorted_ids(p.max_id, p.max_degree as usize, num_largest);
+                let coefficients =
+                    proptest::collection::vec(arbitrary_coefficient_nonzero(), num_largest);
+
+                // The remaining terms
+                let num_sub = p.num_terms - num_largest;
+                let sub = Self::arbitrary_with(PolynomialParameters {
+                    num_terms: num_sub,
+                    max_degree: p.max_degree - 1,
+                    max_id: p.max_id,
+                });
+
+                (ids, coefficients, sub)
+                    .prop_map(|(ids, coefficients, sub)| {
+                        ids.into_iter()
+                            .zip(coefficients)
+                            .chain(sub.into_iter())
+                            .collect()
+                    })
+                    .boxed()
+            })
             .boxed()
     }
 
