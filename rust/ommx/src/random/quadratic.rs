edc28948b6e7fd630e718fd39acc328a8d8c96e8
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-02-26 14:08:06 +0900

    Add methods in QuadraticParameters

--- rust/ommx/src/random/quadratic.rs
+++ rust/ommx/src/random/quadratic.rs
@@ -17,24 +17,72 @@ impl Default for QuadraticParameters {
     }
 }
 
+impl QuadraticParameters {
+    fn possible_max_quad_terms(&self) -> usize {
+        ((self.max_id + 2) * (self.max_id + 1) / 2) as usize
+    }
+
+    fn possible_max_linear_terms(&self) -> usize {
+        (self.max_id + 1) as usize
+    }
+
+    // Possible maximum of the number of terms
+    //
+    // Note that `Quadratic` is not a binary function, x1 * x1 and x1 are different terms.
+    fn possible_max_terms(&self) -> usize {
+        self.possible_max_quad_terms() + self.possible_max_linear_terms()
+    }
+
+    fn linear_terms_range(&self) -> std::ops::RangeInclusive<usize> {
+        let max = std::cmp::min(self.num_terms, self.possible_max_linear_terms());
+        let min = if self.num_terms >= self.possible_max_quad_terms() {
+            self.num_terms - self.possible_max_quad_terms()
+        } else {
+            0
+        };
+        min..=max
+    }
+
+    pub fn smaller(&self) -> impl Strategy<Value = Self> {
+        (0..=self.max_id).prop_flat_map(move |max_id| {
+            let small = Self {
+                max_id,
+                num_terms: 0,
+            };
+            (0..=small.possible_max_terms()).prop_map(move |num_terms| Self { max_id, num_terms })
+        })
+    }
+}
+
 impl Arbitrary for Quadratic {
     type Parameters = QuadraticParameters;
     type Strategy = BoxedStrategy<Self>;
 
-    fn arbitrary_with(
-        QuadraticParameters { num_terms, max_id }: Self::Parameters,
-    ) -> Self::Strategy {
-        (0..=num_terms)
-            .prop_flat_map(move |num_quad| {
-                let num_linear = num_terms - num_quad;
+    fn arbitrary_with(p: Self::Parameters) -> Self::Strategy {
+        assert!(
+            p.num_terms <= p.possible_max_terms(),
+            "num_terms ({num_terms}) must be less than or equal to possible maximum ({possible_max_terms}) determined from max_id ({max_id})",
+            num_terms = p.num_terms,
+            max_id = p.max_id,
+            possible_max_terms = p.possible_max_terms()
+        );
+        p.linear_terms_range()
+            .prop_flat_map(move |num_linear| {
+                let num_quad = p.num_terms - num_linear;
+                assert!(
+                    num_quad <= p.possible_max_quad_terms(),
+                    "num_quad ({num_quad}) must be less than or equal to max_quad_terms({max_quad_terms})",
+                    max_quad_terms = p.possible_max_quad_terms(),
+                );
+
                 let quad = proptest::collection::hash_map(
-                    arbitrary_key(max_id),
+                    arbitrary_key(p.max_id),
                     arbitrary_coefficient_nonzero(),
                     num_quad,
                 );
                 let linear = Linear::arbitrary_with(LinearParameters {
                     num_terms: num_linear,
-                    max_id,
+                    max_id: p.max_id,
                 });
                 (quad, linear).prop_map(|(quad, linear)| {
                     let mut quad: Quadratic = quad.into_iter().collect();
