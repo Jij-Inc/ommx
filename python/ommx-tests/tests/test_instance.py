3f2899ed70f22a8ddb4221603f9368ffb46bba7c
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-03-21 21:19:06 +0900

    Add test case for continuous decision variables and exceeds limit

--- python/ommx-tests/tests/test_instance.py
+++ python/ommx-tests/tests/test_instance.py
@@ -1,4 +1,6 @@
 from ommx.v1 import Instance, DecisionVariable, Function
+import math
+import pytest
 
 
 def test_set_objective():
@@ -13,3 +15,35 @@ def test_set_objective():
 
     instance.objective = x[1]
     assert instance.objective.almost_equal(Function(x[1]))
+
+
+def test_convert_inequality_to_equality_with_integer_slack_variable_limit():
+    x = [DecisionVariable.binary(i) for i in range(3)]
+    instance = Instance.from_components(
+        decision_variables=x,
+        objective=sum(x),
+        constraints=[(math.pi * x[0] + math.e * x[1] >= 1).set_id(0)],
+        sense=Instance.MAXIMIZE,
+    )
+    with pytest.raises(RuntimeError) as e:
+        instance.convert_inequality_to_equality_with_integer_slack_variable(0, 32)
+    assert (
+        str(e.value)
+        == "The range of the slack variable exceeds the limit: evaluated(15174216961756088) > limit(32)"
+    )
+
+
+def test_convert_inequality_to_equality_with_integer_slack_variable_continuous():
+    x = [DecisionVariable.continuous(i, lower=-1.23, upper=4.56) for i in range(3)]
+    instance = Instance.from_components(
+        decision_variables=x,
+        objective=sum(x),
+        constraints=[(x[0] + x[1] >= 7.89).set_id(0)],
+        sense=Instance.MAXIMIZE,
+    )
+    with pytest.raises(RuntimeError) as e:
+        instance.convert_inequality_to_equality_with_integer_slack_variable(0, 32)
+    assert (
+        str(e.value)
+        == "The constraint contains continuous decision variables: ID=VariableID(0)"
+    )
