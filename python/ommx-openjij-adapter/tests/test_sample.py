86953fae86456b4bb9e61ec52125cb2efd600611
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-03-25 21:18:47 +0900

    Tests for binary and integer decision variables with equality and inequality constraints

--- python/ommx-openjij-adapter/tests/test_sample.py
+++ python/ommx-openjij-adapter/tests/test_sample.py
@@ -33,3 +33,90 @@ def test_maximize():
 
     # x0 = x1 = 1 is maximum
     assert sample_set.extract_decision_variables("x", 0) == {(0,): 1.0, (1,): 1.0}
+
+
+def test_binary_equality():
+    x0 = DecisionVariable.binary(0, name="x", subscripts=[0])
+    x1 = DecisionVariable.binary(1, name="x", subscripts=[1])
+    x2 = DecisionVariable.binary(2, name="x", subscripts=[2])
+
+    instance = Instance.from_components(
+        decision_variables=[x0, x1, x2],
+        objective=x0 + 2 * x1 + 3 * x2,
+        constraints=[x1 * x2 == 0],
+        sense=Instance.MAXIMIZE,
+    )
+    sample_set = OMMXOpenJijSAAdapter.sample(
+        instance, num_reads=1, uniform_penalty_weight=3.0, seed=12345
+    )
+
+    # x0 = x2 = 1, x1 = 0 is maximum
+    assert sample_set.extract_decision_variables("x", 0) == {
+        (0,): 1.0,
+        (1,): 0.0,
+        (2,): 1.0,
+    }
+
+def test_binary_inequality():
+    x0 = DecisionVariable.binary(0, name="x", subscripts=[0])
+    x1 = DecisionVariable.binary(1, name="x", subscripts=[1])
+    x2 = DecisionVariable.binary(2, name="x", subscripts=[2])
+
+    instance = Instance.from_components(
+        decision_variables=[x0, x1, x2],
+        objective=x0 + 2 * x1 + 3 * x2,
+        constraints=[x0 + x1 + x2 <= 2],
+        sense=Instance.MAXIMIZE,
+    )
+    sample_set = OMMXOpenJijSAAdapter.sample(
+        instance, num_reads=1, uniform_penalty_weight=3.0, seed=12345
+    )
+
+    # x1 = x2 = 1, x0 = 0 is maximum
+    assert sample_set.extract_decision_variables("x", 0) == {
+        (0,): 0.0,
+        (1,): 1.0,
+        (2,): 1.0,
+    }
+
+
+
+def test_integer_equality():
+    x0 = DecisionVariable.integer(0, name="x", lower=-1, upper=1, subscripts=[0])
+    x1 = DecisionVariable.integer(1, name="x", lower=-1, upper=1, subscripts=[1])
+
+    instance = Instance.from_components(
+        decision_variables=[x0, x1],
+        objective=x0 + 2*x1,
+        constraints=[x0 + x1 == 0],
+        sense=Instance.MAXIMIZE,
+    )
+    sample_set = OMMXOpenJijSAAdapter.sample(
+        instance, num_reads=1, uniform_penalty_weight=3.0, seed=12345
+    )
+
+    # x1 = -x0 = 1 is maximum
+    assert sample_set.extract_decision_variables("x", 0) == {
+        (0,): -1.0,
+        (1,): 1.0,
+    }
+
+def test_integer_inequality():
+    x0 = DecisionVariable.integer(0, name="x", lower=-1, upper=1, subscripts=[0])
+    x1 = DecisionVariable.integer(1, name="x", lower=-1, upper=1, subscripts=[1])
+
+    instance = Instance.from_components(
+        decision_variables=[x0, x1],
+        objective=x0 + 2*x1,
+        constraints=[x0 + x1 <= 0],
+        sense=Instance.MAXIMIZE,
+    )
+    sample_set = OMMXOpenJijSAAdapter.sample(
+        instance, num_reads=1, uniform_penalty_weight=3.0, seed=12345
+    )
+
+    # x1 = -x0 = 1 is maximum
+    assert sample_set.extract_decision_variables("x", 0) == {
+        (0,): -1.0,
+        (1,): 1.0,
+    }
