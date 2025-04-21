7678451f7976f52c6a5ccfdc4e7a486f2137f98a
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-04-21 14:46:26 +0900

    Use Evaluate::required_ids in benchmark code of evaluate.rs

--- rust/ommx/benches/evaluate.rs
+++ rust/ommx/benches/evaluate.rs
@@ -1,50 +1,53 @@
 use criterion::{
     criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion, PlotConfiguration,
 };
-
 use ommx::{
     random::{arbitrary_state, random_deterministic, sample_deterministic, FunctionParameters},
     v1::{Linear, Quadratic},
     Evaluate,
 };
+use proptest::prelude::Arbitrary;
 
-fn evaluate(c: &mut Criterion) {
+fn evaluate<T: Evaluate + Arbitrary<Parameters = FunctionParameters>>(
+    c: &mut Criterion,
+    title: &str,
+    max_degree: u32,
+) {
     let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
-
-    let mut eval_linear = c.benchmark_group("eval-linear");
-    eval_linear.plot_config(plot_config.clone());
-    for num_terms in [1, 10, 100, 1000, 10_000] {
-        let lin: Linear = random_deterministic(FunctionParameters {
+    let mut group = c.benchmark_group(title);
+    group.plot_config(plot_config.clone());
+    for num_terms in [1, 10, 100, 1000, 10_000, 100_000] {
+        let f: T = random_deterministic(FunctionParameters {
             num_terms,
-            max_degree: 1,
+            max_degree,
             max_id: 10 * num_terms as u64,
         });
-        let state = sample_deterministic(arbitrary_state(lin.used_decision_variable_ids()));
-        eval_linear.bench_with_input(
-            BenchmarkId::new("eval-linear", num_terms.to_string()),
-            &(lin, state),
-            |b, (lin, state)| b.iter(|| lin.evaluate(state)),
+        let state = sample_deterministic(arbitrary_state(f.required_ids()));
+        group.bench_with_input(
+            BenchmarkId::new(title, num_terms.to_string()),
+            &(f, state),
+            |b, (f, state)| b.iter(|| f.evaluate(state)),
         );
     }
-    eval_linear.finish();
-
-    let mut eval_quadratic = c.benchmark_group("eval-quadratic");
-    eval_quadratic.plot_config(plot_config.clone());
-    for num_terms in [1, 10, 100, 1000, 10_000] {
-        let quad: Quadratic = random_deterministic(FunctionParameters {
-            num_terms,
-            max_degree: 2,
-            max_id: 10 * num_terms as u64,
-        });
-        let state = sample_deterministic(arbitrary_state(quad.used_decision_variable_ids()));
-        eval_quadratic.bench_with_input(
-            BenchmarkId::new("eval-quadratic", num_terms.to_string()),
-            &(quad, state),
-            |b, (quad, state)| b.iter(|| quad.evaluate(state)),
-        );
-    }
-    eval_quadratic.finish();
+    group.finish();
 }
 
-criterion_group!(benches, evaluate);
+fn evaluate_linear(c: &mut Criterion) {
+    evaluate::<Linear>(c, "evaluate-linear", 1);
+}
+
+fn evaluate_quadratic(c: &mut Criterion) {
+    evaluate::<Quadratic>(c, "evaluate-quadratic", 2);
+}
+
+fn evaluate_polynomial(c: &mut Criterion) {
+    evaluate::<ommx::v1::Polynomial>(c, "evaluate-polynomial", 3);
+}
+
+criterion_group!(
+    benches,
+    evaluate_linear,
+    evaluate_quadratic,
+    evaluate_polynomial
+);
 criterion_main!(benches);
