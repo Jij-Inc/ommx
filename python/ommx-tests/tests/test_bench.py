a0c5da0406d7de8f006054d5fcb0a861ee755b63
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-04-21 22:16:32 +0900

    Benchmark

--- python/ommx-tests/tests/test_bench.py
+++ python/ommx-tests/tests/test_bench.py
@@ -1,6 +1,12 @@
 import pytest
 from statistics import median
 
-@pytest.mark.benchmark
-def test_median_performance():
-    return median([1, 2, 3, 4, 5])
+
+@pytest.mark.parametrize("input_size", [10, 100, 1000])
+def test_to_qubo(benchmark, input_size):
+    def to_qubo(input_size):
+        # Simulate a function that converts to QUBO
+        return median([i for i in range(input_size)])
+
+    result = benchmark(to_qubo, input_size)
+    assert isinstance(result, float)
