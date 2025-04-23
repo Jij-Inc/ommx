6521088112e97950242d1188b139c6c8d4639145
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-04-23 15:32:53 +0900

    Linear::add_term

--- rust/ommx/src/linear/convert.rs
+++ rust/ommx/src/linear/convert.rs
@@ -1,6 +1,20 @@
 use super::*;
 use crate::v1;
 
+impl Linear {
+    pub fn add_term(&mut self, id: VariableID, coefficient: Coefficient) {
+        use std::collections::hash_map::Entry;
+        match self.terms.entry(id) {
+            Entry::Occupied(mut entry) => {
+                *entry.get_mut() += coefficient;
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(coefficient);
+            }
+        }
+    }
+}
+
 impl From<Linear> for v1::Linear {
     fn from(linear: Linear) -> Self {
         let mut new = Self::default();
