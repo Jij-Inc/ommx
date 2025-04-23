a7b759e87fdf249c3a7c943f12a721ece707a008
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-04-23 18:10:34 +0900

    clippy fix

--- rust/ommx/src/linear/convert.rs
+++ rust/ommx/src/linear/convert.rs
@@ -44,8 +44,7 @@ impl Parse for v1::linear::Term {
             Ok(coefficient) => Ok(Some(Term { id, coefficient })),
             Err(CoefficientError::Zero) => Ok(None),
             Err(e) => Err(RawParseError::from(e)
-                .context("ommx.v1.linear.Term", "coefficient")
-                .into()),
+                .context("ommx.v1.linear.Term", "coefficient")),
         }
     }
 }
