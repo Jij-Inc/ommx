5214bd8c8686ef24c77101e09fa0ae0381de0387
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-04-25 18:01:19 +0900

    polynomial/convert.rs

--- rust/ommx/src/polynomial.rs
+++ rust/ommx/src/polynomial.rs
@@ -1,16 +1,57 @@
+mod convert;
+
 use crate::{Coefficient, VariableID};
 use std::{collections::HashMap, hash::Hash};
 
-pub trait Monomial: Hash {}
+/// Monomial
+///
+/// - [`Default`] must return the 0-degree monomial for the constant term
+pub trait Monomial: Hash + Eq + Default {}
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Polynomial<M: Monomial> {
     terms: HashMap<M, Coefficient>,
 }
 
+impl<M: Monomial> Default for Polynomial<M> {
+    fn default() -> Self {
+        Self {
+            terms: HashMap::new(),
+        }
+    }
+}
+
+impl<M: Monomial> Polynomial<M> {
+    pub fn add_term(&mut self, term: M, coefficient: Coefficient) {
+        use std::collections::hash_map::Entry;
+        match self.terms.entry(term) {
+            Entry::Vacant(e) => {
+                e.insert(coefficient);
+            }
+            Entry::Occupied(mut e) => {
+                if let Some(added) = *e.get() + coefficient {
+                    e.insert(added);
+                } else {
+                    e.remove();
+                }
+            }
+        }
+    }
+
+    pub fn contains(&self, term: &M) -> bool {
+        self.terms.contains_key(term)
+    }
+
+    pub fn get(&self, term: &M) -> Option<Coefficient> {
+        self.terms.get(term).copied()
+    }
+}
+
 /// Linear function only contains monomial of degree 1 or constant
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
 pub enum LinearMonomial {
     Variable(VariableID),
+    #[default]
     Constant,
 }
 
