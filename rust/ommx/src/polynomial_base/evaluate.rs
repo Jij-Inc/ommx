ed411f214b54b4a646df7392045fc7011a62582e
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-05-02 13:10:06 +0900

    WIP: Evaluate for PolynomialBase

--- rust/ommx/src/polynomial_base/evaluate.rs
+++ rust/ommx/src/polynomial_base/evaluate.rs
@@ -3,15 +3,28 @@ use crate::{
     v1::{SampledValues, Samples, State},
     Evaluate,
 };
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use std::collections::BTreeSet;
 
 impl<M: Monomial> Evaluate for PolynomialBase<M> {
     type Output = f64;
     type SampledOutput = SampledValues;
 
-    fn evaluate(&self, solution: &State) -> Result<(Self::Output, BTreeSet<u64>)> {
-        todo!()
+    fn evaluate(&self, state: &State) -> Result<(Self::Output, BTreeSet<u64>)> {
+        let mut result = 0.0;
+        let mut ids = BTreeSet::new();
+        for (monomial, coefficient) in self.iter() {
+            let mut out = 1.0;
+            for id in monomial.ids() {
+                out *= state
+                    .entries
+                    .get(&id.into_inner())
+                    .ok_or(anyhow!("Missing entry for id: {}", id.into_inner()))?;
+            }
+            result += coefficient.into_inner() * out;
+            ids.extend(monomial.ids().map(|id| id.into_inner()));
+        }
+        Ok((result, ids))
     }
 
     fn partial_evaluate(&mut self, state: &State) -> Result<BTreeSet<u64>> {
