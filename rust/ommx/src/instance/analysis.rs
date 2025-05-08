8f99d4ef52d3ea24e04627a2f94680ed45c717aa
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-05-08 21:50:53 +0900

    Use FnvHashSet instead of HashSet

--- rust/ommx/src/instance/analysis.rs
+++ rust/ommx/src/instance/analysis.rs
@@ -1,42 +1,42 @@
 use super::*;
 use crate::{Evaluate, Kind};
-use std::collections::HashSet;
+use fnv::FnvHashSet;
 
 #[derive(Debug, Clone, PartialEq, Eq, getset::Getters)]
 pub struct DecisionVariableAnalysis {
     #[getset(get = "pub")]
-    binary: HashSet<VariableID>,
+    binary: FnvHashSet<VariableID>,
     #[getset(get = "pub")]
-    integer: HashSet<VariableID>,
+    integer: FnvHashSet<VariableID>,
     #[getset(get = "pub")]
-    continuous: HashSet<VariableID>,
+    continuous: FnvHashSet<VariableID>,
     #[getset(get = "pub")]
-    semi_integer: HashSet<VariableID>,
+    semi_integer: FnvHashSet<VariableID>,
     #[getset(get = "pub")]
-    semi_continuous: HashSet<VariableID>,
+    semi_continuous: FnvHashSet<VariableID>,
 
     /// The set of decision variables that are used in the objective function.
     #[getset(get = "pub")]
-    used_in_objective: HashSet<VariableID>,
+    used_in_objective: FnvHashSet<VariableID>,
     /// The set of decision variables that are used in the constraints.
     #[getset(get = "pub")]
-    used_in_constraints: HashSet<VariableID>,
+    used_in_constraints: FnvHashSet<VariableID>,
 
     /// Dependent variables
     #[getset(get = "pub")]
-    dependent: HashSet<VariableID>,
+    dependent: FnvHashSet<VariableID>,
     /// The decision variables that used for evaluating the dependent variables.
     #[getset(get = "pub")]
-    dependency_source: HashSet<VariableID>,
+    dependency_source: FnvHashSet<VariableID>,
 }
 
 impl Instance {
     pub fn analyze_decision_variables(&self) -> DecisionVariableAnalysis {
-        let mut binary = HashSet::new();
-        let mut integer = HashSet::new();
-        let mut continuous = HashSet::new();
-        let mut semi_integer = HashSet::new();
-        let mut semi_continuous = HashSet::new();
+        let mut binary = FnvHashSet::default();
+        let mut integer = FnvHashSet::default();
+        let mut continuous = FnvHashSet::default();
+        let mut semi_integer = FnvHashSet::default();
+        let mut semi_continuous = FnvHashSet::default();
         for (id, dv) in &self.decision_variables {
             match dv.kind {
                 Kind::Binary => binary.insert(*id),
@@ -46,11 +46,24 @@ impl Instance {
                 Kind::SemiContinuous => semi_continuous.insert(*id),
             };
         }
-        let used_in_objective = self.objective.required_ids();
-        let mut used_in_constraints = HashSet::new();
+        let used_in_objective = self
+            .objective
+            .required_ids()
+            .into_iter()
+            .map(VariableID::from)
+            .collect();
+        let mut used_in_constraints = FnvHashSet::default();
         for constraint in self.constraints.values() {
-            used_in_constraints.extend(constraint.function.required_ids());
+            used_in_constraints.extend(
+                constraint
+                    .function
+                    .required_ids()
+                    .into_iter()
+                    .map(VariableID::from),
+            );
         }
+        let dependent = FnvHashSet::default();
+        let dependency_source = FnvHashSet::default();
 
         DecisionVariableAnalysis {
             binary,
