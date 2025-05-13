f7a0f1da3fa623aadcf22fab958894c387e97db7
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-05-13 16:52:52 +0900

    Constraint::{partial_evaluate, evaluate_samples};

--- rust/ommx/src/constraint/evaluate.rs
+++ rust/ommx/src/constraint/evaluate.rs
@@ -3,6 +3,7 @@ use crate::{
     v1::{EvaluatedConstraint, SampledConstraint},
     Evaluate, FnvHashMapExt,
 };
+use std::collections::HashMap;
 
 impl Evaluate for Constraint {
     type Output = EvaluatedConstraint;
@@ -30,11 +31,35 @@ impl Evaluate for Constraint {
         &self,
         samples: &crate::v1::Samples,
     ) -> anyhow::Result<Self::SampledOutput> {
-        todo!()
+        let evaluated_values = self.function.evaluate_samples(samples)?;
+        let feasible: HashMap<u64, bool> = evaluated_values
+            .iter()
+            .map(|(sample_id, value)| match self.equality {
+                Equality::EqualToZero => {
+                    return (*sample_id, value.abs() < 1e-6);
+                }
+                Equality::LessThanOrEqualToZero => {
+                    return (*sample_id, *value < 1e-6);
+                }
+            })
+            .collect();
+        Ok(SampledConstraint {
+            id: self.id.into_inner(),
+            evaluated_values: Some(evaluated_values),
+            used_decision_variable_ids: self.function.required_ids().into_iter().collect(),
+            name: self.name.clone(),
+            subscripts: self.subscripts.clone(),
+            parameters: self.parameters.to_std(),
+            description: self.description.clone(),
+            equality: self.equality.into(),
+            feasible,
+            removed_reason: None,
+            removed_reason_parameters: Default::default(),
+        })
     }
 
     fn partial_evaluate(&mut self, state: &crate::v1::State) -> anyhow::Result<()> {
-        todo!()
+        self.function.partial_evaluate(state)
     }
 
     fn required_ids(&self) -> std::collections::BTreeSet<u64> {
