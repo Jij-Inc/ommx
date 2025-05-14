5188e8bc7a1760cfe4e862b41fa1fc96c99f1320
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-05-15 01:31:01 +0900

    Getters for Instance

--- rust/ommx/src/instance.rs
+++ rust/ommx/src/instance.rs
@@ -28,15 +28,24 @@ pub enum Sense {
 /// - Key of `constraints` and `removed_constraints` are disjoint.
 /// - The keys of `decision_variable_dependency` are also keys of `decision_variables`.
 ///
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, getset::Getters)]
 pub struct Instance {
+    #[getset(get = "pub")]
     sense: Sense,
+    #[getset(get = "pub")]
     objective: Function,
+    #[getset(get = "pub")]
     decision_variables: BTreeMap<VariableID, DecisionVariable>,
+    #[getset(get = "pub")]
     constraints: BTreeMap<ConstraintID, Constraint>,
+    #[getset(get = "pub")]
     removed_constraints: BTreeMap<ConstraintID, RemovedConstraint>,
+    #[getset(get = "pub")]
     decision_variable_dependency: BTreeMap<VariableID, Function>,
+    #[getset(get = "pub")]
     parameters: Option<v1::Parameters>,
+    #[getset(get = "pub")]
     description: Option<v1::instance::Description>,
+    #[getset(get = "pub")]
     constraint_hints: ConstraintHints,
 }
