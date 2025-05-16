836d80644dd030007b3342aaf69d09e9082e6298
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-05-16 20:58:13 +0900

    Convert of DecisionVariable

--- rust/ommx/src/decision_variable/parse.rs
+++ rust/ommx/src/decision_variable/parse.rs
@@ -22,6 +22,32 @@ impl Parse for v1::decision_variable::Kind {
     }
 }
 
+impl TryFrom<v1::decision_variable::Kind> for Kind {
+    type Error = ParseError;
+    fn try_from(value: v1::decision_variable::Kind) -> Result<Self, Self::Error> {
+        value.parse(&())
+    }
+}
+
+impl From<Kind> for v1::decision_variable::Kind {
+    fn from(kind: Kind) -> Self {
+        use v1::decision_variable::Kind::*;
+        match kind {
+            Kind::Continuous => Continuous,
+            Kind::Integer => Integer,
+            Kind::Binary => Binary,
+            Kind::SemiContinuous => SemiContinuous,
+            Kind::SemiInteger => SemiInteger,
+        }
+    }
+}
+
+impl From<Kind> for i32 {
+    fn from(kind: Kind) -> Self {
+        v1::decision_variable::Kind::from(kind) as i32
+    }
+}
+
 impl Parse for v1::DecisionVariable {
     type Output = DecisionVariable;
     type Context = ();
@@ -43,6 +69,13 @@ impl Parse for v1::DecisionVariable {
     }
 }
 
+impl TryFrom<v1::DecisionVariable> for DecisionVariable {
+    type Error = ParseError;
+    fn try_from(value: v1::DecisionVariable) -> Result<Self, Self::Error> {
+        value.parse(&())
+    }
+}
+
 impl Parse for Vec<v1::DecisionVariable> {
     type Output = BTreeMap<VariableID, DecisionVariable>;
     type Context = ();
@@ -56,5 +89,31 @@ impl Parse for Vec<v1::DecisionVariable> {
             }
         }
         Ok(decision_variables)
+    }
+}
+
+impl From<DecisionVariable> for v1::DecisionVariable {
+    fn from(
+        DecisionVariable {
+            id,
+            kind,
+            bound,
+            substituted_value,
+            name,
+            subscripts,
+            parameters,
+            description,
+        }: DecisionVariable,
+    ) -> Self {
+        Self {
+            id: id.into_inner(),
+            kind: kind.into(),
+            bound: Some(bound.into()),
+            substituted_value,
+            name,
+            subscripts,
+            parameters: parameters.into_iter().collect(),
+            description,
+        }
     }
 }
