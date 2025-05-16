9e94130946737a7dfc78a47d819ad98b52cc47dd
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-05-16 17:53:54 +0900

    WIP: Evaluate DecisionVariable

--- rust/ommx/src/instance/evaluate.rs
+++ rust/ommx/src/instance/evaluate.rs
@@ -3,46 +3,39 @@ use crate::{
     v1::{SampleSet, Solution},
     Evaluate, VariableIDSet,
 };
-use anyhow::{bail, Result};
+use anyhow::Result;
 
 impl Evaluate for Instance {
     type Output = Solution;
     type SampledOutput = SampleSet;
 
     fn evaluate(&self, state: &v1::State, atol: f64) -> Result<Self::Output> {
-        let analysis = self.analyze_decision_variables();
-        // Check bounds and integrality
+        let state = self
+            .analyze_decision_variables()
+            .populate(state.clone(), atol)?;
 
-        // Use required IDs to evaluate the objective and constraints
-        let objective = self.objective.evaluate(state, atol)?;
+        let objective = self.objective.evaluate(&state, atol)?;
         let evaluated_constraints = self
             .constraints
             .iter()
-            .map(|(_id, constraint)| constraint.evaluate(state, atol))
+            .map(|(_id, constraint)| constraint.evaluate(&state, atol))
             .collect::<Result<Vec<_>>>()?;
 
-        let mut state = state.clone();
-        // Check fixed variables are consistent
-        for (id, value) in analysis.fixed() {
-            if let Some(v) = state.entries.get(id) {
-                if (v - value).abs() > 1e-6 {
-                    bail!("Inconsistent fixed variable: {id} = {value}, but found {v} in state");
-                }
-            } else {
-                // TODO: Check bound
-                state.entries.insert(id.into_inner(), *value);
-            }
-        }
-        // TODO: Fix a possible value for irrelevant variables
-        for id in analysis.irrelevant() {
-            todo!()
-        }
-        // TODO: Fill dependent variables
+        let decision_variables = self
+            .decision_variables
+            .values()
+            .map(|dv| {
+                let id = dv.id.into_inner();
+                let value = state.entries.get(&id).unwrap(); // Safe unwrap, as we populate the state with the decision variables
+                todo!()
+            })
+            .collect();
 
         Ok(Solution {
             state: Some(state.clone()),
             objective,
             evaluated_constraints,
+            decision_variables,
             ..Default::default()
         })
     }
