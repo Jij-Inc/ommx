77709ae5679c0d305514418367d4e8857c7e4284
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-05-27 15:26:43 +0900

    impl SubstituteWithLinears for QuadraticMonomial

--- rust/ommx/src/polynomial_base/substitute.rs
+++ rust/ommx/src/polynomial_base/substitute.rs
@@ -1,6 +1,6 @@
 use crate::{
     substitute::{ClassifiedAssignments, LinearAssignments, Substitute, SubstituteWithLinears},
-    Linear, LinearMonomial, Monomial, Polynomial, PolynomialBase,
+    Linear, LinearMonomial, Monomial, Polynomial, PolynomialBase, Quadratic, QuadraticMonomial,
 };
 
 impl<M> Substitute for PolynomialBase<M>
@@ -29,6 +29,26 @@ impl SubstituteWithLinears for LinearMonomial {
                 }
             }
             LinearMonomial::Constant => Linear::one(),
+        }
+    }
+}
+
+impl SubstituteWithLinears for QuadraticMonomial {
+    type Output = Quadratic;
+
+    fn substitute_with_linears(&self, linear_assignments: &LinearAssignments) -> Self::Output {
+        match self {
+            QuadraticMonomial::Pair(pair) => {
+                let l_sub = LinearMonomial::Variable(pair.lower())
+                    .substitute_with_linears(linear_assignments);
+                let u_sub = LinearMonomial::Variable(pair.upper())
+                    .substitute_with_linears(linear_assignments);
+                (&l_sub * &u_sub).into()
+            }
+            QuadraticMonomial::Linear(id) => LinearMonomial::Variable(*id)
+                .substitute_with_linears(linear_assignments)
+                .into(),
+            QuadraticMonomial::Constant => Quadratic::one(),
         }
     }
 }
