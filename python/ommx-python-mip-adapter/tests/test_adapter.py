ac3fbf4a68af67d14dbaf6c2d76d9b6b01789fc1
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-06-13 12:21:20 +0900

    More idiomatic test

--- python/ommx-python-mip-adapter/tests/test_adapter.py
+++ python/ommx-python-mip-adapter/tests/test_adapter.py
@@ -1,19 +1,15 @@
 import pytest
-
-from ommx.v1 import Instance, DecisionVariable, Constraint
-from ommx.v1 import Function, Quadratic
-
+from ommx.v1 import Instance, DecisionVariable
 from ommx_python_mip_adapter import OMMXPythonMIPAdapter
-
 from ommx_python_mip_adapter.exception import OMMXPythonMIPAdapterError
 
 
 def test_error_nonlinear_objective():
     # Objective function: 2.3 * x * x (variable ID should match)
-    quadratic = Quadratic(columns=[0], rows=[0], values=[2.3])
+    x = DecisionVariable.continuous(0)
     ommx_instance = Instance.from_components(
-        decision_variables=[DecisionVariable.continuous(0)],
-        objective=Function(quadratic),
+        decision_variables=[x],
+        objective=2.3 * x * x,
         constraints=[],
         sense=Instance.MINIMIZE,
     )
@@ -26,27 +22,14 @@ def test_error_nonlinear_objective():
 def test_error_nonlinear_constraint():
     # Objective function: 0
     # Constraint: 2.3 * x * x = 0
-    quadratic = Quadratic(columns=[1], rows=[1], values=[2.3])
-    constraint = Constraint(
-        id=0,
-        function=Function(quadratic),
-        equality=Constraint.EQUAL_TO_ZERO,
-    )
+    x = DecisionVariable.continuous(1)
     ommx_instance = Instance.from_components(
-        decision_variables=[DecisionVariable.continuous(1)],
-        objective=Function(0),
-        constraints=[constraint],
+        decision_variables=[x],
+        objective=0.0,
+        constraints=[(2.3 * x * x == 0).set_id(0)],
         sense=Instance.MINIMIZE,
     )
 
     with pytest.raises(OMMXPythonMIPAdapterError) as e:
         OMMXPythonMIPAdapter(ommx_instance)
     assert "The function must be either `constant` or `linear`." in str(e.value)
-
-
-# NOTE: This test case is commented out because the new API doesn't allow
-# creating constraints with invalid equality values through factory methods.
-# The validation happens at creation time, so this error case is no longer reachable.
-# def test_error_not_supported_constraint_equality():
-#     # This test would require creating a constraint with EQUALITY_UNSPECIFIED
-#     # which is not possible with the new factory method API
