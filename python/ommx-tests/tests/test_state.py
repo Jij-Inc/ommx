87ce8adcb73b9890b71dfcaa12d7099ffa9bb584
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-06-17 20:28:53 +0900

    Format

--- python/ommx-tests/tests/test_state.py
+++ python/ommx-tests/tests/test_state.py
@@ -1,6 +1,7 @@
 from ommx.v1 import State
 import pytest
 
+
 def test_create_state_from_dict():
     # dict[int, float]
     state = State({1: 0.0, 2: 1.0, 3: 0.0, 4: 1.0, 5: 1.0})
@@ -14,6 +15,7 @@ def test_create_state_from_dict():
     state = State({1: 0.0, 2: 1, 3: 0.0, 4: 1, 5: 1})
     assert len(state.entries) == 5
 
+
 def test_create_state_from_list():
     state = State([(1, 0.0), (2, 1.0), (3, 0.0), (4, 1.0), (5, 1.0)])
     assert len(state.entries) == 5
@@ -24,12 +26,18 @@ def test_create_state_from_list():
     state = State([(1, 0.0), (2, 1.0), (3, 0), (4, 1), (5, 1)])
     assert len(state.entries) == 5
 
+
 def test_create_size_mismatch():
     with pytest.raises(TypeError) as e:
         _state = State([(1, 0.0, 1.0)])
-    assert str(e.value) == "ommx.v1.State can only be initialized with a `dict[int, float]` or `Iterable[tuple[int, float]]`"
+    assert (
+        str(e.value)
+        == "ommx.v1.State can only be initialized with a `dict[int, float]` or `Iterable[tuple[int, float]]`"
+    )
 
     with pytest.raises(TypeError) as e:
         _state = State((1, 0.0))
-    assert str(e.value) == "ommx.v1.State can only be initialized with a `dict[int, float]` or `Iterable[tuple[int, float]]`"
-
+    assert (
+        str(e.value)
+        == "ommx.v1.State can only be initialized with a `dict[int, float]` or `Iterable[tuple[int, float]]`"
+    )
