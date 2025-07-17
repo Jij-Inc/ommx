a19291d8df8a6c1a22d928374f5fa7491ab79339
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-07-17 20:18:50 +0900

    clippy fix

--- rust/ommx/src/instance/convert.rs
+++ rust/ommx/src/instance/convert.rs
@@ -80,7 +80,7 @@ impl ParametricInstance {
             let missing_ids: Vec<_> = required_ids.difference(&given_ids).collect();
             for id in &missing_ids {
                 if let Some(param) = self.parameters.get(id) {
-                    log::error!("Missing parameter: {:?}", param);
+                    log::error!("Missing parameter: {param:?}");
                 }
             }
             bail!(
