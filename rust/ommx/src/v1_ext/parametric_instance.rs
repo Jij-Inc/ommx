c51c954c5a32925692bbfb3446f53d2b31846c49
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-07-18 12:54:53 +0900

    Drop impl Evaluate for v1::Constraint

--- rust/ommx/src/v1_ext/parametric_instance.rs
+++ rust/ommx/src/v1_ext/parametric_instance.rs
@@ -46,47 +46,6 @@ impl From<Parameters> for State {
 }
 
 impl ParametricInstance {
-    /// Create a new [Instance] with the given parameters.
-    pub fn with_parameters(
-        mut self,
-        parameters: Parameters,
-        atol: crate::ATol,
-    ) -> Result<Instance> {
-        let required_ids: BTreeSet<u64> = self.parameters.iter().map(|p| p.id).collect();
-        let given_ids: BTreeSet<u64> = parameters.entries.keys().cloned().collect();
-        if !required_ids.is_subset(&given_ids) {
-            for ids in required_ids.difference(&given_ids) {
-                let parameter = self.parameters.iter().find(|p| p.id == *ids).unwrap();
-                log::error!("Missing parameter: {parameter:?}");
-            }
-            bail!(
-                "Missing parameters: Required IDs {:?}, got {:?}",
-                required_ids,
-                given_ids
-            );
-        }
-
-        let state = State::from(parameters.clone());
-        if let Some(f) = self.objective.as_mut() {
-            f.partial_evaluate(&state, atol)?;
-        }
-        for constraint in self.constraints.iter_mut() {
-            constraint.partial_evaluate(&state, atol)?;
-        }
-
-        Ok(Instance {
-            description: self.description,
-            objective: self.objective,
-            constraints: self.constraints,
-            decision_variables: self.decision_variables,
-            sense: self.sense,
-            parameters: Some(parameters),
-            constraint_hints: self.constraint_hints,
-            removed_constraints: self.removed_constraints,
-            decision_variable_dependency: self.decision_variable_dependency,
-        })
-    }
-
     pub fn objective(&self) -> Cow<Function> {
         match &self.objective {
             Some(f) => Cow::Borrowed(f),
@@ -157,30 +116,5 @@ impl ParametricInstance {
             }
         }
         Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use approx::abs_diff_eq;
-    use proptest::prelude::*;
-
-    proptest! {
-        #[test]
-        fn test_parametric_instance_conversion(instance in Instance::arbitrary()) {
-            let parametric_instance: ParametricInstance = instance.clone().into();
-            let converted_instance: Instance = parametric_instance.with_parameters(Parameters::default(), crate::ATol::default()).unwrap();
-            prop_assert_eq!(&converted_instance.parameters, &Some(Parameters::default()));
-            prop_assert!(
-                abs_diff_eq!(instance, converted_instance),
-                "\nLeft : {:?}\nRight: {:?}", instance, converted_instance
-            );
-        }
-
-        #[test]
-        fn validate(pi in ParametricInstance::arbitrary()) {
-            pi.validate().unwrap();
-        }
     }
 }
