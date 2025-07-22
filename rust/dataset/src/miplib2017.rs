8b61de7815ab0673624421a0fbe97f2948fcdcb4
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-07-22 20:55:40 +0900

    Revise error handling, push artifacts

--- rust/dataset/src/miplib2017.rs
+++ rust/dataset/src/miplib2017.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use ommx::artifact::Builder;
 use std::{fs, path::Path};
 use zip::ZipArchive;
@@ -6,9 +6,10 @@ use zip::ZipArchive;
 pub fn package(path: &Path) -> Result<()> {
     let annotation_dict = ommx::dataset::miplib2017::instance_annotations();
     log::info!("Input Archive: {}", path.display());
-    let f = fs::File::open(path)?;
-    let mut ar = ZipArchive::new(f)?;
+    let f = fs::File::open(path).with_context(|| format!("File not found: {path:?}"))?;
+    let mut ar = ZipArchive::new(f).with_context(|| format!("Not a ZIP archive: {path:?}"))?;
 
+    // Input archive is expected to contain `*.mps.gz` files on the root level.
     for i in 0..ar.len() {
         let file = ar.by_index(i)?;
         let Some(name) = file.name().strip_suffix(".mps.gz").map(str::to_string) else {
@@ -32,10 +33,11 @@ pub fn package(path: &Path) -> Result<()> {
             annotations.variables()?,
             "Variable count mismatch for {name}"
         );
-        // let mut builder = Builder::for_github("Jij-Inc", "ommx", "miplib2017", &name)?;
-        // builder.add_instance(instance.into(), annotations.clone())?;
-        // let mut _artifact = builder.build()?;
-        // artifact.push()?;
+
+        let mut builder = Builder::for_github("Jij-Inc", "ommx", "miplib2017", &name)?;
+        builder.add_instance(instance.into(), annotations.clone())?;
+        let mut artifact = builder.build()?;
+        artifact.push()?;
     }
     Ok(())
 }
