d9acecdbb9d076511e96b394742ce44969c48c2c
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-07-30 15:13:32 +0900

    Show diff of proptest for Instance roundtrip

--- rust/ommx/src/mps/tests/write_arbitrary.rs
+++ rust/ommx/src/mps/tests/write_arbitrary.rs
@@ -2,6 +2,23 @@ use super::super::*;
 use crate::Instance;
 use approx::AbsDiffEq;
 use proptest::prelude::*;
+use similar::{ChangeTag, TextDiff};
+
+fn take_diff(expected: &Instance, actual: &Instance) -> String {
+    let expected = format!("{:#?}", expected);
+    let actual = format!("{:#?}", actual);
+    let diff = TextDiff::from_lines(&expected, &actual);
+    let mut diff_string = String::new();
+    for change in diff.iter_all_changes() {
+        let sign = match change.tag() {
+            ChangeTag::Delete => "-",
+            ChangeTag::Insert => "+",
+            ChangeTag::Equal => " ",
+        };
+        diff_string += &format!("{}{}", sign, change);
+    }
+    diff_string
+}
 
 proptest! {
     #[test]
@@ -15,6 +32,10 @@ proptest! {
         let mut buffer = Vec::new();
         prop_assert!(format::format(&instance, &mut buffer).is_ok());
         let loaded = parse(&buffer[..]).unwrap();
-        prop_assert!(instance.abs_diff_eq(&loaded, crate::ATol::default()));
+        prop_assert!(
+            instance.abs_diff_eq(&loaded, crate::ATol::default()),
+            "Instance not matching after roundtrip:\n{}",
+            take_diff(&instance, &loaded)
+        );
     }
 }
