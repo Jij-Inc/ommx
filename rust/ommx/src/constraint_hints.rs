90127ea11ab367d73ee4d164eacf0160f7c99369
Toshiki Teramura <toshiki.teramura@gmail.com>
2025-08-15 21:00:38 +0900

    Split one_hot and sos1 constraints into separate files for better organization and maintainability.

--- rust/ommx/src/constraint_hints.rs
+++ rust/ommx/src/constraint_hints.rs
@@ -1,99 +1,14 @@
+mod one_hot;
+mod sos1;
+
+pub use one_hot::OneHot;
+pub use sos1::Sos1;
+
 use crate::{
-    parse::{as_constraint_id, as_variable_id, Parse, ParseError, RawParseError},
-    v1::{self},
-    Constraint, ConstraintID, DecisionVariable, InstanceError, RemovedConstraint, VariableID,
+    parse::{Parse, ParseError},
+    v1, Constraint, ConstraintID, DecisionVariable, RemovedConstraint, VariableID,
 };
-use std::collections::{BTreeMap, BTreeSet};
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct OneHot {
-    pub id: ConstraintID,
-    pub variables: BTreeSet<VariableID>,
-}
-
-impl Parse for v1::OneHot {
-    type Output = OneHot;
-    type Context = (
-        BTreeMap<VariableID, DecisionVariable>,
-        BTreeMap<ConstraintID, Constraint>,
-        BTreeMap<ConstraintID, RemovedConstraint>,
-    );
-    fn parse(
-        self,
-        (decision_variable, constraints, removed_constraints): &Self::Context,
-    ) -> Result<Self::Output, ParseError> {
-        let message = "ommx.v1.OneHot";
-        let constraint_id = as_constraint_id(constraints, removed_constraints, self.constraint_id)
-            .map_err(|e| e.context(message, "constraint_id"))?;
-        let mut variables = BTreeSet::new();
-        for v in &self.decision_variables {
-            let id = as_variable_id(decision_variable, *v)
-                .map_err(|e| e.context(message, "decision_variables"))?;
-            if !variables.insert(id) {
-                return Err(
-                    RawParseError::InstanceError(InstanceError::NonUniqueVariableID { id })
-                        .context(message, "decision_variables"),
-                );
-            }
-        }
-        Ok(OneHot {
-            id: constraint_id,
-            variables,
-        })
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Sos1 {
-    pub binary_constraint_id: ConstraintID,
-    pub big_m_constraint_ids: BTreeSet<ConstraintID>,
-    pub variables: BTreeSet<VariableID>,
-}
-
-impl Parse for v1::Sos1 {
-    type Output = Sos1;
-    type Context = (
-        BTreeMap<VariableID, DecisionVariable>,
-        BTreeMap<ConstraintID, Constraint>,
-        BTreeMap<ConstraintID, RemovedConstraint>,
-    );
-    fn parse(
-        self,
-        (decision_variable, constraints, removed_constraints): &Self::Context,
-    ) -> Result<Self::Output, ParseError> {
-        let message = "ommx.v1.Sos1";
-        let binary_constraint_id =
-            as_constraint_id(constraints, removed_constraints, self.binary_constraint_id)
-                .map_err(|e| e.context(message, "binary_constraint_id"))?;
-        let mut big_m_constraint_ids = BTreeSet::new();
-        for id in &self.big_m_constraint_ids {
-            let id = as_constraint_id(constraints, removed_constraints, *id)
-                .map_err(|e| e.context(message, "big_m_constraint_ids"))?;
-            if !big_m_constraint_ids.insert(id) {
-                return Err(
-                    RawParseError::InstanceError(InstanceError::NonUniqueConstraintID { id })
-                        .context(message, "big_m_constraint_ids"),
-                );
-            }
-        }
-        let mut variables = BTreeSet::new();
-        for id in &self.decision_variables {
-            let id = as_variable_id(decision_variable, *id)
-                .map_err(|e| e.context(message, "decision_variables"))?;
-            if !variables.insert(id) {
-                return Err(
-                    RawParseError::InstanceError(InstanceError::NonUniqueVariableID { id })
-                        .context(message, "decision_variables"),
-                );
-            }
-        }
-        Ok(Sos1 {
-            binary_constraint_id,
-            big_m_constraint_ids,
-            variables,
-        })
-    }
-}
+use std::collections::BTreeMap;
 
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct ConstraintHints {
@@ -130,25 +45,6 @@ impl Parse for v1::ConstraintHints {
             one_hot_constraints,
             sos1_constraints,
         })
-    }
-}
-
-impl From<OneHot> for v1::OneHot {
-    fn from(value: OneHot) -> Self {
-        Self {
-            constraint_id: *value.id,
-            decision_variables: value.variables.into_iter().map(|v| *v).collect(),
-        }
-    }
-}
-
-impl From<Sos1> for v1::Sos1 {
-    fn from(value: Sos1) -> Self {
-        Self {
-            binary_constraint_id: *value.binary_constraint_id,
-            big_m_constraint_ids: value.big_m_constraint_ids.into_iter().map(|c| *c).collect(),
-            decision_variables: value.variables.into_iter().map(|v| *v).collect(),
-        }
     }
 }
 
